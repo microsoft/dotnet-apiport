3b8c8a83548d330801115328ef3c558b1a17d2d2
Connie Yau <conniey@microsoft.com>
2015-02-19 00:42:49 -0800

    Add pre-build script to install KRE and KPM

---
+++ build/install-kre.ps1
@@ -0,0 +1,16 @@
+﻿$kpmPath = Join-Path "$env:USERPROFILE" ".kpm"
+$krePath = Join-Path "$env:USERPROFILE" ".kre"
+
+$isKPMExist = Test-Path $kpmPath -PathType Container
+$isKREExist = Test-Path $krePath -PathType Container
+
+if ($isKPMExist -and $isKREExist)
+{
+    Write-Host "KPM [$kpmPath] and KRE [$krePath] exists."
+    return;
+}
+
+& powershell -NoProfile -ExecutionPolicy Unrestricted -Command "Invoke-Expression ((New-Object Net.WebClient).DownloadString('https://raw.githubusercontent.com/aspnet/Home/master/kvminstall.ps1'))"
+
+$KREInstallPath = "$($env:USERPROFILE)\.kre\bin"
+& powershell -NoProfile -ExecutionPolicy Unrestricted -Command "& $KREInstallPath\kvm.cmd upgrade"
