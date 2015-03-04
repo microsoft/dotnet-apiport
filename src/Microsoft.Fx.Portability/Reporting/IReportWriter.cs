fdb07aaf2292fd62108f4a03bb8e3145266ee63c
Taylor Southwick <tasou@microsoft.com>
2015-03-04 11:03:43 -0800

    Add IReportWriter interface

---
+++ src/Microsoft.Fx.Portability/Reporting/IReportWriter.cs
@@ -0,0 +1,15 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Microsoft.Fx.Portability.ObjectModel;
+using Microsoft.Fx.Portability.Reporting.ObjectModel;
+using System.IO;
+
+namespace Microsoft.Fx.Portability.Reporting
+{
+    public interface IReportWriter
+    {
+        ResultFormat Format { get; }
+        void WriteStream(Stream stream, AnalyzeResponse response);
+    }
+}
