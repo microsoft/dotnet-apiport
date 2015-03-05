0de1a6bcb7e7af58a6b080316ac8008558a66ba7
Connie Yau <conniey@microsoft.com>
2015-03-05 01:38:54 -0800

    Modify after-build to support more than one library being built

--- build/after-build.ps1
+++ build/after-build.ps1
@@ -1,8 +1,7 @@
 ﻿param(
     [Parameter(Mandatory=$true, Position = 0)][string]$BinariesDirectory,
     [Parameter(Mandatory=$true, Position = 1)][string]$BuildVersion,
-    [Parameter(Mandatory=$true, Position = 2)][string]$BuildDefinitionName,
-    [switch]$IsDebug
+    [Parameter(Mandatory=$true, Position = 2)][string]$BuildDefinitionName
 )
 
 function Get-VersionNumber
@@ -54,87 +53,111 @@ function Replace-NuspecTokens($ProjectOutputDirectory, $NuspecFile)
     $result | Set-Content $NuspecFile
 }
 
-function Get-ProjectName($ProjectOutputDirectory)
+function Get-ProjectName($NuGetPackage)
 {
-    $allDllsAndPdbs = Get-ChildItem $ProjectOutputDirectory.FullName -Recurse | ? { $_.Extension -eq '.dll' -or $_.Extension -eq '.pdb' }
-    $uniqueNames = $allDllsAndPdbs | select BaseName -Unique
-
-    if (@($uniqueNames).Count -ne 1)
+    if ($NuGetPackage.BaseName -match '(?<projectname>[a-zA-Z\.]+)\.(?<version>[0-9\.]+(\-\w+)?)')
     {
-        Write-Error "There should only been 1 dll/pdb name... Actual [$uniqueNames]" -ErrorAction Stop
+        return $Matches['projectname']
+    }
+    else
+    {
+        Write-Error "[$($NuGetPackage.BaseName)] should have matched the Regex!"
+        return $null
+    }
+}
+
+function Invoke-DownloadNuget([string]$OutputDirectory)
+{
+    if (!(Test-Path $OutputDirectory))
+    {
+        New-Item $OutputDirectory -ItemType Directory | Out-Null
     }
 
-    return $uniqueNames[0].BaseName
+    $nugetExe = Join-Path $OutputDirectory "nuget.exe"
+
+    Invoke-WebRequest "http://www.nuget.org/nuget.exe" -OutFile $nugetExe -ErrorAction Stop
+
+    return $nugetExe
 }
 
 $versionNumber = Get-VersionNumber
 
-# For each project we want to
+$nugetDirectory = Join-Path $env:TEMP $(Get-Random)
+$nugetExe = Invoke-DownloadNuget $nugetDirectory
+
+# For each project we want to get the nupkgs and for each of them:
 # 1) Unpack the .nupkg
 # 2) Merge the corresponding .nuspec with the .nuspec in the .nupkg
 # 3) Repack the .nupkg with the signed dlls
 foreach ($projectDirectory in $(Get-ChildItem $BinariesDirectory | ? { $_.PSIsContainer }))
 {
-    $allNupkgs = Get-ChildItem $projectDirectory.FullName -Recurse | ? { $_.Extension -eq '.nupkg' }
-    $excludingSymbolNupkgs = $allNupkgs | ? { !$_.Name.Contains("symbols") } 
-    $nupkg = $excludingSymbolNupkgs| select -First 1
-
-    if (@($excludingSymbolNupkgs).Count -gt 1)
+    $allNupkgs = Get-ChildItem $projectDirectory.FullName -Recurse | ? { $_.Extension -eq '.nupkg' -and !$_.Name.Contains("symbols") }
+    
+    foreach ($nupkg in $allNupkgs)
     {
-        Write-Error "There should only be 1 nupkg for each project [$projectDirectory]. Actual [$excludingSymbolNupkgs]"
-        foreach ($pkg in $excludingSymbolNupkgs)
+        $projectName = Get-ProjectName $nupkg
+        $originalNuspec = Join-Path $PSScriptRoot "$projectName.nuspec"
+
+        if (!(Test-Path $originalNuspec))
         {
-            Write-Output "Package: $($pkg.FullName)"
+            Write-Host "A template nuspec does not exist for the project [$projectName], skipping unpacking... Template [$originalNuspec]"
+            continue
+        }
+        
+        $tempDirectory = Join-Path $env:TEMP $(Get-Random)
+        $unzippedDirectory = Join-Path $tempDirectory "Unzipped"
+        $zipFile = Join-Path $tempDirectory "$($nupkg.BaseName).zip"
+
+        # Expand-ZipFile only works with .zip
+        New-Item $tempDirectory -ItemType Directory | Out-Null
+        Copy-Item $nupkg.FullName $zipFile
+
+        $resultingNuspecFile = Join-Path $projectDirectory.FullName "$projectName.nuspec"
+        
+        Copy-Item $originalNuspec $resultingNuspecFile
+
+        Expand-ZipFile $zipFile $unzippedDirectory -ErrorAction Stop
+    
+        # Getting both .nuspec files, the template and the one from kpm build to merge.
+        $sourceNuspecFile = Get-ChildItem $unzippedDirectory -Recurse -Include *.nuspec
+        if (@($sourceNuspecFile).Count -ne 1)
+        {
+            Write-Error "ERROR: There should be 1 nuspec file in [$tempDirectory]! Actual [$sourceNuspecFile]"
+            continue
         }
 
-        continue
+        $sourceNuspecFile = $sourceNuspecFile | select -First 1
+
+        Replace-NuspecTokens $projectDirectory $resultingNuspecFile
+    
+        Add-VersionToNuspec $sourceNuspecFile.FullName $versionNumber
+
+        Join-XmlFiles -Source $sourceNuspecFile.FullName -Template $resultingNuspecFile -Result $resultingNuspecFile
+
+        # Move the original nupkgs and its symbol into another folder.
+        $originalNupkgsDirectory = Join-Path $projectDirectory.FullName "originalnupkgs"
+        
+        if (!(Test-Path $originalNupkgsDirectory -PathType Container))
+        {
+            New-Item $originalNupkgsDirectory -ItemType Directory | Out-Null
+        }
+
+        $nupkg | Move-Item -Destination $originalNupkgsDirectory
+        
+        $nupkgSymbol = Join-Path $nupkg.Directory "$($nupkg.BaseName).symbols$($nupkg.Extension)"
+        $nupkgSymbol | Move-Item -Destination $originalNupkgsDirectory
+
+        $symbolPackage = Join-Path $nupkg.Directory "$($nupkg.BaseName).symbols$($nupkg.Extension)"
+        $symbolPackage | Move-Item -Destination $originalNupkgsDirectory
+
+        # Finally, repack all of the contents of the nupkg.
+        Invoke-Expression "$nugetExe pack $resultingNuspecFile -OutputDirectory $($projectDirectory.FullName) -Symbols" -ErrorAction Stop
+
+        # Cleaning up after ourselves...
+        Remove-Item $resultingNuspecFile -Force
+        Remove-Item $tempDirectory -Recurse -Force
     }
-    elseif (@($excludingSymbolNupkgs).Count -eq 0)
-    {
-        continue
-    }
-    
-    $projectName = Get-ProjectName $projectDirectory
-    $tempDirectory = Join-Path $env:TEMP $(Get-Random)
-    $zipFile = Join-Path $tempDirectory "$($nupkg.BaseName).zip"
-
-    # Expand-ZipFile only works with .zip
-    New-Item $tempDirectory -ItemType Directory | Out-Null
-    Copy-Item $nupkg.FullName $zipFile
-
-    $resultingNuspecFile = Join-Path $projectDirectory.FullName "$projectName.nuspec"
-    $originalNuspec = Join-Path $PSScriptRoot "$projectName.nuspec"
-    $unzippedDirectory = Join-Path $tempDirectory "Unzipped"
-    
-    Copy-Item $originalNuspec $resultingNuspecFile
-    Expand-ZipFile $zipFile $unzippedDirectory -ErrorAction Stop
-    
-    # Getting both .nuspec files, the template and the one from kpm build to merge.
-    $sourceNuspecFile = Get-ChildItem $unzippedDirectory -Recurse -Include *.nuspec
-    if (@($sourceNuspecFile).Count -ne 1)
-    {
-        Write-Error "ERROR: There should be 1 nuspec file in [$tempDirectory]! Actual [$sourceNuspecFile]"
-        continue
-    }
-
-    $sourceNuspecFile = $sourceNuspecFile | select -First 1
-
-    Replace-NuspecTokens $projectDirectory $resultingNuspecFile
-    
-    Add-VersionToNuspec $sourceNuspecFile.FullName $versionNumber
-
-    Join-XmlFile -SourceFile $sourceNuspecFile.FullName -TargetFile $resultingNuspecFile -ResultFile $resultingNuspecFile
-
-    # Move all of the original nupkgs into another folder.
-    $originalNupkgsDirectory = Join-Path $projectDirectory.FullName "originalnupkgs"
-    
-    New-Item $originalNupkgsDirectory -ItemType Directory | Out-Null
-    $allNupkgs | Move-Item -Destination $originalNupkgsDirectory
-
-    # Finally, repack all of the contents of the nupkg.
-    Invoke-Expression "$PSScriptRoot\nuget.exe pack $resultingNuspecFile -OutputDirectory $($projectDirectory.FullName) -Symbols" -ErrorAction Stop
-
-    # Cleaning up after ourselves...
-    Remove-Item $resultingNuspecFile -Force
-    Remove-Item $tempDirectory -Recurse -Force
 }
+
+# Remove the downloaded nuget.exe
+Remove-Item $nugetDirectory -Recurse -Force
