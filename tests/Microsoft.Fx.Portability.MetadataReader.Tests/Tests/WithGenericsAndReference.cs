cd0302675bbdf10e99e15702baf5bf1e5e27c572
Taylor Southwick <tasou@microsoft.com>
2015-04-15 08:26:28 -0700

    Add some basic MetadataReader tests

---
+++ tests/Microsoft.Fx.Portability.MetadataReader.Tests/Tests/WithGenericsAndReference.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace WithGenericsAndReference
+{
+    class WithGenericsAndReferenceTest
+    {
+        static void Main(string[] args)
+        {
+            // Reference EmptyProject
+            TestAssembly1.EmptyProject.Main(new[] { "hello" });
+
+            Handler += Program_Handler;
+        }
+
+        static void Program_Handler(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        static void TestGeneric<T>(T item)
+        {
+            Console.WriteLine(item);
+
+            Handler(null, new EventArgs());
+        }
+
+        public static event EventHandler Handler;
+    }
+}
