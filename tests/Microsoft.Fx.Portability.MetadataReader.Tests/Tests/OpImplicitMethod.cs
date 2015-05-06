fc44212e4f2b27248bdf343392f3be0f3cef1488
Taylor Southwick <tasou@microsoft.com>
2015-05-06 14:26:39 -0700

    Add check for op_Implicit and op_Explicit specialname
    This adds a heuristic to determine if op_Implicit and op_Explicit are
    special names by determining how many parameters they have and
    restricting it to be a special name if it only has one input parameter
    
    Fixes #51

---
+++ tests/Microsoft.Fx.Portability.MetadataReader.Tests/Tests/OpImplicitMethod.cs
@@ -0,0 +1,23 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace Microsoft.Fx.Portability.MetadataReader.Tests
+{
+    internal class OpImplicit_Method
+    {
+        private static void Main(string[] args)
+        {
+            var method = new OpImplicit_Method<int>();
+
+            method.op_Implicit(1);
+        }
+    }
+
+    internal struct OpImplicit_Method<T>
+    {
+        public int op_Implicit(T other)
+        {
+            return 0;
+        }
+    }
+}
