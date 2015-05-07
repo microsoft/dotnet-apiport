cb935be491132170ea3896c6b9240fffb944ac76
Mike Rousos <MikeRou@microsoft.com>
2015-05-07 13:02:08 -0700

    Initial port of Microsoft.Fx.Portability.Offline, Microsoft.Fx.Portability.Reports.Html, and Microsoft.Fx.Portability.Reports.Json.

---
+++ src/Microsoft.Fx.Portability.Reports.Json/JsonReportWriter.cs
@@ -0,0 +1,39 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Microsoft.Fx.Portability.ObjectModel;
+using Microsoft.Fx.Portability.Reporting;
+using Newtonsoft.Json;
+using System.IO;
+
+namespace Microsoft.Fx.Portability.Reports
+{
+    public class JsonReportWriter : IReportWriter
+    {
+        private readonly ResultFormatInformation _formatInformation;
+
+        public JsonReportWriter()
+        {
+            _formatInformation = new ResultFormatInformation
+            {
+                DisplayName = "Json",
+                MimeType = "application/json",
+                FileExtension = ".json"
+            };
+        }
+
+        public ResultFormatInformation Format
+        {
+            get { return _formatInformation; }
+        }
+
+        public void WriteStream(Stream stream, AnalyzeResponse response)
+        {
+            using (var streamWriter = new StreamWriter(stream))
+            using (var writer = new JsonTextWriter(streamWriter))
+            {
+                DataExtensions.Serializer.Serialize(writer, response);
+            }
+        }
+    }
+}
