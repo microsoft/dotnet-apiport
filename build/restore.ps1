3be5463db77b971f678340dde8a0cacc2299624a
Taylor Southwick <tasou@microsoft.com>
2015-09-18 12:09:33 -0700

    Move catalog downloading to restore step

--- build/restore.ps1
+++ build/restore.ps1
@@ -2,3 +2,6 @@ $root = $PSScriptRoot
 $nuget = & "$root\Get-Nuget.ps1"
 
 & $nuget restore $root\..\PortabilityTools.sln
+
+# Restore data for offline lib (cannot do this in the csproj due to race conditions)
+& $root\Get-CatalogFile.ps1 $root\..\.data\catalog.bin
