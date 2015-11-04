c14196402814c2eb7aae75540128cd9caf45c1b7
Taylor Southwick <tasou@microsoft.com>
2015-11-04 12:32:27 -0800

    Create directory so log can be written

--- build.cmd
+++ build.cmd
@@ -6,6 +6,8 @@ SET VersionSuffix=alpha
 :: Default to a release build
 IF "%Configuration%"=="" SET Configuration=Release
 
+if not exist bin\%Configuration% mkdir bin\%Configuration%
+
 powershell -noprofile -executionpolicy bypass -file build\restore.ps1
 "%ProgramFiles(x86)%\MSBuild\14.0\bin\MSBuild.exe" /nologo /m /v:m /nr:false /flp:logfile=bin\%Configuration%\msbuild.log;verbosity=normal %*
 powershell -noprofile -executionpolicy bypass -file build\postbuild.ps1 -configuration %Configuration%
