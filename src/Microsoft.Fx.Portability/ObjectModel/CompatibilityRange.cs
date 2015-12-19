82a9aa9c945073dc53a52cfaadacd1e6f5aa8be4
Jon Jung <jjvertical@hotmail.com>
2015-12-18 18:39:59 -0800

    Add DiagnosticAnalyzerInfo and CompatibilityRange classes

---
+++ src/Microsoft.Fx.Portability/ObjectModel/CompatibilityRange.cs
@@ -0,0 +1,20 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+
+namespace Microsoft.Fx.Portability.ObjectModel
+{
+#if FEATURE_SERIALIZABLE
+    [Serializable]
+#endif
+    public class CompatibilityRange
+    {
+        public string FrameworkName { get; set; }
+        public Version StartVersion { get; set; }
+        public Version EndVersion { get; set; }
+        public Version LatestVersion { get; set; }
+        public Version DefaultVersion { get; set; }
+        public bool IsRetargeting { get; set; }
+    }
+}
