833851a32a5796c37e5a565d62d13c16812c9682
Taylor Southwick <tasou@microsoft.com>
2016-01-29 13:24:42 -0800

    Add AncestorApiRecommendations
    This enable offline mode to use the hierarchy of docids

--- src/Microsoft.Fx.Portability.Offline/OfflineApiRecommendations.cs
+++ src/Microsoft.Fx.Portability.Offline/OfflineApiRecommendations.cs
@@ -8,40 +8,21 @@ using System.Linq;
 
 namespace Microsoft.Fx.Portability
 {
-    public class OfflineApiRecommendations : IApiRecommendations
+    public class OfflineApiRecommendations : AncestorApiRecommendations
     {
         private IEnumerable<BreakingChange> _breakingChanges;
         private IApiCatalogLookup _lookup;
 
         public OfflineApiRecommendations(IApiCatalogLookup lookup, IEnumerable<BreakingChange> breakingChanges)
+            : base(lookup)
         {
             _lookup = lookup;
             _breakingChanges = breakingChanges;
         }
 
-        public IEnumerable<ApiNote> GetNotes(string docId)
-        {
-            return Enumerable.Empty<ApiNote>();
-        }
-
-        public IEnumerable<BreakingChange> GetBreakingChanges(string docId)
+        protected override IEnumerable<BreakingChange> GetBreakingChanges(string docId)
         {
             return _breakingChanges.Where(b => b.ApplicableApis.Contains(docId, StringComparer.Ordinal));
-        }
-
-        public string GetRecommendedChanges(string docId)
-        {
-            return _lookup.GetRecommendedChange(docId);
-        }
-
-        public string GetSourceCompatibleChanges(string docId)
-        {
-            return _lookup.GetSourceCompatibilityEquivalent(docId);
-        }
-
-        public string GetComponent(string docId)
-        {
-            return string.Empty;
         }
     }
 }
