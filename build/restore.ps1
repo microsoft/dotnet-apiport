0f5dc68ec04322e784bae34a4c1cc88cea3f38e7
Connie Yau <conniey@microsoft.com>
2016-03-07 14:32:18 -0800

    Restoring packages in package directory in solution

--- build/restore.ps1
+++ build/restore.ps1
@@ -1,7 +1,14 @@
 $root = $PSScriptRoot
+$packages = "$root\..\packages"
+
 $nuget = & "$root\Get-Nuget.ps1"
 
-& $nuget restore $root\..\PortabilityTools.sln
+New-Item $packages -ItemType Directory -ErrorAction Ignore
+
+# Restoring NuGet packages into this specific directory because the automated
+# build definition needs to be able to find the Visual Studio Test Adapter 
+# (xunit.runner.visualstudio) in a known location
+& $nuget restore $root\..\PortabilityTools.sln -PackagesDirectory $packages
 
 # Restore data for offline lib (cannot do this in the csproj due to race conditions)
 & $root\Get-CatalogFile.ps1 $root\..\.data\catalog.bin
