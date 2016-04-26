888ce4946830ff3c83d2a96ee805e266a0c9b1de
Taylor Southwick <tasou@microsoft.com>
2016-04-26 10:14:30 -0700

    Allow package restore location to be set in a way that gets propagated where it is needed

--- build/restore.ps1
+++ build/restore.ps1
@@ -1,14 +1,17 @@
 $root = $PSScriptRoot
-$packages = "$root\..\packages"
-
 $nuget = & "$root\Get-Nuget.ps1"
 
-New-Item $packages -ItemType Directory -ErrorAction Ignore
-
 # Restoring NuGet packages into this specific directory because the automated
-# build definition needs to be able to find the Visual Studio Test Adapter 
+# build definition in VSTS needs to be able to find the Visual Studio Test Adapter 
 # (xunit.runner.visualstudio) in a known location
-& $nuget restore $root\..\PortabilityTools.sln -PackagesDirectory $packages
+if($env:NuGetPackagesDirectory)
+{
+	& $nuget restore $root\..\PortabilityTools.sln -PackagesDirectory $env:NuGetPackagesDirectory
+}
+else
+{
+	& $nuget restore $root\..\PortabilityTools.sln
+}
 
 # Restore data for offline lib (cannot do this in the csproj due to race conditions)
 & $root\Get-CatalogFile.ps1 $root\..\.data\catalog.bin
