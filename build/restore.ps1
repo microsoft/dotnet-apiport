b61adf0290975ee262bf08e41e20e2f3f5f85642
Connie Yau <conniey@microsoft.com>
2016-10-09 23:57:09 -0700

    Adding ability to publish vsix

--- build/restore.ps1
+++ build/restore.ps1
@@ -1,17 +1,43 @@
+[CmdletBinding(DefaultParameterSetName="All")]
+param(
+    [Parameter(Position = 0, Mandatory = "true", ParameterSetName="PublishVsix")]
+    [int]$BuildNumber,
+
+    [ValidateSet("build","revision")]
+    [Parameter(Position = 1, Mandatory = "true", ParameterSetName="PublishVsix")]
+    [string]$VersionType = "revision",
+
+    [Parameter(Mandatory = "true", ParameterSetName="PublishVsix")]
+    [switch]$PublishVsix
+)
+
+$ErrorActionPreference = "Stop"
+
+$IsPublishVsix = $PsCmdlet.ParameterSetName -eq "PublishVsix"
+
 $root = $PSScriptRoot
-$nuget = & "$root\Get-Nuget.ps1"
+$buildToolScript = Join-Path $root "Get-BuildTools.ps1"
+$nuget = & $buildToolScript "nuget"
+$portabilitySolution = $(Resolve-Path "$root\..\PortabilityTools.sln").Path
 
 # Restoring NuGet packages into this specific directory because the automated
 # build definition in VSTS needs to be able to find the Visual Studio Test Adapter 
 # (xunit.runner.visualstudio) in a known location
 if($env:NuGetPackagesDirectory)
 {
-	& $nuget restore $root\..\PortabilityTools.sln -PackagesDirectory $env:NuGetPackagesDirectory
+    & $nuget restore $portabilitySolution -PackagesDirectory $env:NuGetPackagesDirectory
 }
 else
 {
-	& $nuget restore $root\..\PortabilityTools.sln
+    & $nuget restore $portabilitySolution
 }
 
 # Restore data for offline lib (cannot do this in the csproj due to race conditions)
 & $root\Get-CatalogFile.ps1 $root\..\.data\catalog.bin
+
+if ($IsPublishVsix) {
+
+    . $(& $buildToolScript "vsix")
+
+    Vsix-IncrementVsixVersion -buildNumber $BuildNumber -versionType $VersionType | Vsix-UpdateBuildVersion
+}
