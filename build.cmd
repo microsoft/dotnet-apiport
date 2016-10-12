1f8bb1b2d882a54ebf30aa2801076fa719839103
Connie Yau <conniey@microsoft.com>
2016-10-11 17:55:49 -0700

    Consolidating build.cmd

--- build.cmd
+++ build.cmd
@@ -6,8 +6,6 @@ SET VersionSuffix=alpha
 :: Default to a release build
 IF "%Configuration%"=="" SET Configuration=Release
 
-if not exist bin\%Configuration% mkdir bin\%Configuration%
-
-powershell -noprofile -executionpolicy bypass -file build\restore.ps1
-"%ProgramFiles(x86)%\MSBuild\14.0\bin\MSBuild.exe" /nologo /m /v:m /nr:false /flp:logfile=bin\%Configuration%\msbuild.log;verbosity=normal %*
-powershell -noprofile -executionpolicy bypass -file build\postbuild.ps1 -configuration %Configuration%
+powershell -noprofile -executionpolicy bypass -file build.ps1 %Configuration% "AnyCPU" -RunTests -CreateNugetPackages -VersionSuffix %VersionSuffix%
+powershell -noprofile -executionpolicy bypass -file build.ps1 %Configuration% "x64" -VersionSuffix %VersionSuffix%
+powershell -noprofile -executionpolicy bypass -file build.ps1 %Configuration% "x86" -VersionSuffix %VersionSuffix%
