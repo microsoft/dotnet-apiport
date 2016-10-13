78cf7f08e2f5374ec21a3cfdc26338887a96c0db
Connie Yau <conniey@microsoft.com>
2016-10-13 11:06:03 -0700

    Adding CI build (#352)
    * Adding ability to build and test using powershell
    
    * Adding netci.groovy for CI build
    
    * Consolidating build.cmd

---
+++ build.ps1
@@ -0,0 +1,51 @@
+﻿[CmdletBinding()] # Needed to support -Verbose
+param(
+    [Parameter(Position = 0, Mandatory=$true)]
+    [ValidateSet("Release","Debug")]
+    [string]$Configuration,
+
+    [Parameter(Position = 1, Mandatory=$true)]
+    [ValidateSet("AnyCPU", "x86", "x64")]
+    [string]$Platform,
+
+    [Parameter(Position = 2)]
+    [ValidateSet("quiet", "minimal", "normal","diagnostic")]
+    [string]$Verbosity = "normal",
+
+    [switch]$RunTests,
+    [switch]$CreateNugetPackages,
+
+    [string]$VersionSuffix = "alpha"
+)
+
+$ErrorActionPreference = "Stop"
+
+# Libraries are currently pre-release
+$env:VersionSuffix = $VersionSuffix
+
+$binFolder = [IO.Path]::Combine("bin", $Configuration)
+
+if (!(Test-Path $binFolder)) {
+    New-Item $binFolder -ItemType Directory
+}
+
+.\build\restore.ps1
+
+$MSBuild = Join-Path ${env:ProgramFiles(x86)} "MSBuild\14.0\bin\MSBuild.exe"
+
+# PortabilityTools.sln understands "Any CPU" not "AnyCPU"
+$PlatformToUse = $Platform
+
+if ($Platform -eq "AnyCPU") {
+    $PlatformToUse = "Any CPU"
+}
+
+& $MSBuild PortabilityTools.sln /p:Configuration=$Configuration /p:Platform="$PlatformToUse" /nologo /m /v:m /nr:false /flp:logfile=$binFolder\msbuild.log`;verbosity=$Verbosity
+
+if ($RunTests) {
+    .\build\runtests.ps1 $Configuration 
+}
+
+if ($CreateNugetPackages) {
+    .\build\postbuild.ps1 $Configuration
+}
