e4200a5c7a25d34bf385d6491af78e7ac42e4c95
Connie Yau <conniey@microsoft.com>
2017-02-09 02:09:03 -0800

    Adding support for proxy settings.

---
+++ src/Microsoft.Fx.Portability/Proxy/IProxyProvider.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Net;
+
+namespace Microsoft.Fx.Portability.Proxy
+{
+    /// <summary>
+    /// Coordinates the proxy settings and credentials
+    /// </summary>
+    public interface IProxyProvider
+    {
+        /// <summary>
+        /// The credential provider to fetch credentials
+        /// </summary>
+        ICredentialProvider CredentialProvider { get; }
+
+        /// <summary>
+        /// The resolved proxy based on the destination Uri.
+        /// </summary>
+        IWebProxy GetProxy(Uri sourceUri);
+
+        /// <summary>
+        /// Updates the existing credentials in the proxy.
+        /// </summary>
+        void UpdateProxyCredentials(NetworkCredential credentials);
+    }
+}
