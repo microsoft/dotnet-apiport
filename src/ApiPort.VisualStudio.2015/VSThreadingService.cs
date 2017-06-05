df396bf97b0739e3996ba54dbe889c81bee30225
Connie Yau <conniey@microsoft.com>
2017-06-05 14:07:38 -0700

    Add VsThreadingService for 2015

---
+++ src/ApiPort.VisualStudio.2015/VSThreadingService.cs
@@ -0,0 +1,23 @@
+﻿using ApiPortVS.Contracts;
+using Microsoft.VisualStudio.ProjectSystem;
+using System.Threading;
+using System.Threading.Tasks;
+using System;
+
+namespace ApiPortVS.VS2015
+{
+    public class VSThreadingService : IVSThreadingService
+    {
+        private readonly IThreadHandling _threadHandling;
+
+        public VSThreadingService(IThreadHandling threadHandling)
+        {
+            _threadHandling = threadHandling;
+        }
+
+        public async Task SwitchToMainThreadAsync(CancellationToken cancellationToken = default(CancellationToken))
+        {
+            await _threadHandling.AsyncPump.SwitchToMainThreadAsync(cancellationToken);
+        }
+    }
+}
