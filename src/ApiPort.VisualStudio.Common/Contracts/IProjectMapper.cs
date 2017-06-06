90953299789f106e42a228d906e4677af3961c6a
Connie Yau <conniey@microsoft.com>
2017-06-06 13:43:47 -0700

    Adding IProjectMapper

---
+++ src/ApiPort.VisualStudio.Common/Contracts/IProjectMapper.cs
@@ -0,0 +1,14 @@
+﻿using EnvDTE;
+using Microsoft.VisualStudio.Shell.Interop;
+using System.Threading.Tasks;
+
+namespace ApiPortVS.Contracts
+{
+    /// <summary>
+    /// Maps a <see cref="Project"/> to its associated COM types in Visual Studio
+    /// </summary>
+    public interface IProjectMapper
+    {
+        Task<IVsHierarchy> GetVsHierarchyAsync(Project project);
+    }
+}
