f4ca85de0a84cb65be9d4f7dc72f7e17c9e66d0a
Connie Yau <conniey@microsoft.com>
2017-06-06 11:52:17 -0700

    Implementing IProjectBuilder in 2015 and 2017

---
+++ src/ApiPort.VisualStudio.2015/ProjectBuilder.cs
@@ -0,0 +1,15 @@
+﻿using ApiPortVS.Contracts;
+using EnvDTE;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ApiPortVS.VS2015
+{
+    public class ProjectBuilder : IProjectBuilder
+    {
+        public Task<bool> BuildAsync(IEnumerable<Project> projects) => throw new NotImplementedException();
+        public Task<IEnumerable<string>> GetBuildOutputFilesAsync(Project project, CancellationToken cancellationToken = default(CancellationToken)) => throw new NotImplementedException();
+    }
+}
---
+++ src/ApiPort.VisualStudio.2017/ProjectBuilder.cs
@@ -0,0 +1,26 @@
+﻿using ApiPortVS.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EnvDTE;
+using Microsoft.VisualStudio.Shell.Interop;
+using System.Threading;
+
+namespace ApiPortVS.VS2017
+{
+    public class ProjectBuilder : IProjectBuilder
+    {
+        private readonly IVSThreadingService _threadingService;
+
+        public ProjectBuilder(IVSThreadingService threadingService)
+        {
+            _threadingService = threadingService;
+        }
+
+        public Task<bool> BuildAsync(IEnumerable<Project> projects) => throw new NotImplementedException();
+        public Task<IEnumerable<string>> GetBuildOutputFilesAsync(Project project, CancellationToken cancellationToken = default(CancellationToken)) => throw new NotImplementedException();
+        public Task<IVsHierarchy> GetVsHierarchyAsync(Project project) => throw new NotImplementedException();
+    }
+}
