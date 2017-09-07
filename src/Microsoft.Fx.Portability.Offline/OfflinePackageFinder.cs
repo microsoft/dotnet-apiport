a1e65f389417bb6ef7335ee3eb479cae4f943a06
Alina Popa <alpopa@microsoft.com>
2017-09-07 12:05:28 -0700

    Add implementation of IPackageFinder for offline mode.

---
+++ src/Microsoft.Fx.Portability.Offline/OfflinePackageFinder.cs
@@ -0,0 +1,18 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Runtime.Versioning;
+
+namespace Microsoft.Fx.Portability.ObjectModel
+{
+    public class OfflinePackageFinder : IPackageFinder
+    {
+        public bool TryFindPackage(string assemblyInfo, IEnumerable<FrameworkName> targets, out ImmutableDictionary<FrameworkName, IEnumerable<NuGetPackageId>> packages)
+        {
+            packages = ImmutableDictionary<FrameworkName, IEnumerable<NuGetPackageId>>.Empty;
+            return false;
+        }
+    }
+}
