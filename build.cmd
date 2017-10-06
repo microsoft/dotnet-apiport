a7cf6a0ebccf4c971da7bd6e51cbdc2a22cec9e4
Taylor Southwick <tasou@microsoft.com>
2017-10-06 11:10:50 -0700

    Use GitVersion for versioning

--- build.cmd
+++ build.cmd
@@ -1,9 +1,6 @@
 @ECHO OFF
 
-:: Libraries are currently pre-release
-SET VersionSuffix=alpha
-
 :: Default to a release build
 IF "%Configuration%"=="" SET Configuration=Release
 
-powershell -noprofile -executionpolicy bypass -file .\build.ps1 %Configuration% "AnyCPU" -RunTests -VersionSuffix %VersionSuffix%
+powershell -noprofile -executionpolicy bypass -file .\build.ps1 %Configuration% "AnyCPU" -RunTests
