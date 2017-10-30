45c96c76dfbd7654d0cd66a02ffd1a293b293fee
Connie Yau <conniey@microsoft.com>
2017-10-29 22:03:50 -0700

    Adding SystemObjectNotFoundException

---
+++ src/Microsoft.Fx.Portability.MetadataReader/Exceptions/SystemObjectNotFoundException.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Fx.Portability.Analyzer.Resources;
+using Microsoft.Fx.Portability.ObjectModel;
+using System.Collections.Generic;
+
+namespace Microsoft.Fx.Portability.Analyzer.Exceptions
+{
+    /// <summary>
+    /// Exception thrown when assembly containing <see cref="System.Object"/>
+    /// cannot be found.
+    /// </summary>
+    public class SystemObjectNotFoundException : PortabilityAnalyzerException
+    {
+        public SystemObjectNotFoundException(IEnumerable<AssemblyReferenceInformation> assemblies)
+            : base(string.Format(LocalizedStrings.MissingSystemObjectAssembly,
+                string.Join(", ", assemblies)))
+        { }
+    }
+}
