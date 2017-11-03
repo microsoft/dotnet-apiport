ed9dd0a5b5ac2c133b8041c4aa62143e15adb117
Jeremy Meng <yumeng@microsoft.com>
2017-11-03 16:28:02 -0700

    Use a string constant for the https scheme

--- src/Microsoft.Fx.Portability/ApiPortService.cs
+++ src/Microsoft.Fx.Portability/ApiPortService.cs
@@ -25,6 +25,8 @@ namespace Microsoft.Fx.Portability
             internal const string DefaultResultFormat = "/api/resultformat/default";
         }
 
+        const string UriScheme = "https";
+
         private static readonly TimeSpan Timeout = TimeSpan.FromMinutes(10);
         private readonly CompressedHttpClient _client;
 
@@ -39,7 +41,7 @@ namespace Microsoft.Fx.Portability
                 throw new ArgumentNullException(nameof(proxyProvider));
             }
 
-            var uri = new UriBuilder("https", endpoint).Uri;
+            var uri = new UriBuilder(UriScheme, endpoint).Uri;
             var proxy = proxyProvider.GetProxy(uri);
             
             // replace the handler with the proxy aware handler
@@ -59,7 +61,7 @@ namespace Microsoft.Fx.Portability
 
             _client = new CompressedHttpClient(info, messageHandler)
             {
-                BaseAddress = new UriBuilder("https", endpoint).Uri,
+                BaseAddress = new UriBuilder(UriScheme, endpoint).Uri,
                 Timeout = Timeout
             };
         }
@@ -73,7 +75,7 @@ namespace Microsoft.Fx.Portability
 
             _client = new CompressedHttpClient(info)
             {
-                BaseAddress = new UriBuilder("https", endpoint).Uri,
+                BaseAddress = new UriBuilder(UriScheme, endpoint).Uri,
                 Timeout = Timeout
             };
         }
@@ -92,7 +94,7 @@ namespace Microsoft.Fx.Portability
 
             _client = new CompressedHttpClient(info, httpMessageHandler)
             {
-                BaseAddress = new UriBuilder("https", endpoint).Uri,
+                BaseAddress = new UriBuilder(UriScheme, endpoint).Uri,
                 Timeout = Timeout
             };
         }
