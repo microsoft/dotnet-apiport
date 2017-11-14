50c66c6936ea80ecc4a089e1b983d5f63d7728cf
Connie Yau <conniey@microsoft.com>
2017-11-14 15:26:36 -0800

    Fixes #503 (#504)

--- init.ps1
+++ init.ps1
@@ -1,6 +1,6 @@
 function DownloadFile($url, $outputPath) {
 	Write-Host "Attempt to download to $outputPath"
-	
+
 	# If the file has been downloaded don't download again. An empty file implies a failed download
 	if(Test-Path $outputPath) {
 		$file = Get-ChildItem $outputPath
@@ -10,13 +10,13 @@ function DownloadFile($url, $outputPath) {
 			return;
 		}
 	}
-	
+
 	try {
 		# Create placeholder so directory exists
 		New-Item -Type File $OutputPath -Force | Out-Null
-		
+
 		# Attempt to download.  If fails, placeholder remains so msbuild won't complain
-		Invoke-WebRequest $url -OutFile $OutputPath | Out-Null 
+		Invoke-WebRequest $url -OutFile $OutputPath | Out-Null
 
 		Write-Host "Downloaded $OutputPath"
 	} catch {
@@ -24,4 +24,7 @@ function DownloadFile($url, $outputPath) {
 	}
 }
 
-DownloadFile "https://portabilitystorage.blob.core.windows.net/catalog/catalog.bin?sr=c&sv=2015-02-21&si=ReadCatalog&sig=8tOHoX2ZvcSFLol0GI6lxmydNPJbnJdHNLKr06aD7t4%3D"  "$PSScriptRoot\.data\catalog.bin"
+$address = "https://dotnetportability.blob.core.windows.net/catalog/catalog.bin"
+$token = "?sv=2015-04-05&sr=b&si=ReadCatalog&sig=4Qfue7TXbeyS9w3kDp9%2BA6TskFhVb5uW97IE7AVI5SA%3D&st=2017-11-14T21%3A34%3A39Z&se=2018-11-14T21%3A34%3A39Z"
+
+DownloadFile "$address$token" "$PSScriptRoot\.data\catalog1.bin"
