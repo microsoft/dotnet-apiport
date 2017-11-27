27e968203f20416dace2e453c35bb57bebf81380
Taylor Southwick <tasou@microsoft.com>
2017-11-27 17:39:55 +0000

    Repro

---
+++ test-assets/multiple-mscorlib/ConsoleApp1LibPCL/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleApp1LibPCL
+{
+    public class Class1
+    {
+       public Func<string> ReturnSomething
+       {
+          get { return () => "I'm returning something. Woot!"; }
+       }
+    }
+}
