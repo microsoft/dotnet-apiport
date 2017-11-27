23a5bdef23597f9a9463a74114e64033f2f08890
Taylor Southwick <tasou@microsoft.com>
2017-11-27 17:45:57 +0000

    Add repro

---
+++ test-assets/multiple-mscorlib/multiple-mscorlib-dep/PortableRepro.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MultipleMscorlib
+{
+    public class PortableImpl
+    {
+        public Func<string> ReturnSomething
+        {
+            get { return () => string.Empty; }
+        }
+    }
+}
