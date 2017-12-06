bb14d67d0cf659049ae21828b889a1ce3e486e2c
Charles Lowell <chlowe@microsoft.com>
2017-12-06 15:20:46 -0800

    Render all sections of HTML report in test

--- tests/Microsoft.Fx.Portability.Reports.Html.Tests/HtmlReportWriterTests.cs
+++ tests/Microsoft.Fx.Portability.Reports.Html.Tests/HtmlReportWriterTests.cs
@@ -23,6 +23,8 @@ namespace Microsoft.Fx.Portability.Reports.Html.Tests
         [InlineData("_Scripts.cshtml")]
         [InlineData("_Styles.cshtml")]
         [InlineData("_BreakingChangesReport.cshtml")]
+        [InlineData("_CompatibilityResults.cshtml")]
+        [InlineData("_CompatibilitySummary.cshtml")]
         [Theory]
         public void CanFindTemplates(string templateName)
         {
@@ -39,21 +41,15 @@ namespace Microsoft.Fx.Portability.Reports.Html.Tests
             var mapper = Substitute.For<ITargetMapper>();
             var writer = new HtmlReportWriter(mapper);
 
-            var response = new AnalyzeResponse
-            {
-                MissingDependencies = new List<MemberInfo>
-                {
-                    new MemberInfo { MemberDocId = "Type1.doc1", DefinedInAssemblyIdentity = "Assembly1", TypeDocId = "Type1" },
-                    new MemberInfo { MemberDocId = "Type2.doc2", DefinedInAssemblyIdentity = "Assembly2", TypeDocId = "Type2" }
-                },
-                SubmissionId = Guid.NewGuid().ToString(),
-                Targets = new List<FrameworkName> { new FrameworkName("target1", Version.Parse("1.0.0.0")) },
-                UnresolvedUserAssemblies = new List<string> { "UnresolvedAssembly", "UnresolvedAssembly2", "UnresolvedAssembly3" },
-                BreakingChangeSkippedAssemblies = new List<AssemblyInfo>(),
-                BreakingChanges = new List<BreakingChangeDependency>(),
-            };
+            var response = GetAnalyzeResponse();
 
-            var reportingResult = new ReportingResult(response.Targets, response.MissingDependencies, response.SubmissionId, AnalyzeRequestFlags.NoTelemetry);
+            // setting all show... flags renders every section of the report
+            var flags = AnalyzeRequestFlags.NoTelemetry
+                      | AnalyzeRequestFlags.ShowBreakingChanges
+                      | AnalyzeRequestFlags.ShowNonPortableApis
+                      | AnalyzeRequestFlags.ShowRetargettingIssues;
+
+            var reportingResult = new ReportingResult(response.Targets, response.MissingDependencies, response.SubmissionId, flags);
             response.ReportingResult = reportingResult;
 
             var tempFile = Path.GetTempFileName();
@@ -80,5 +76,64 @@ namespace Microsoft.Fx.Portability.Reports.Html.Tests
                 }
             }
         }
+
+        // an AnalyzeResponse with data for every part of the report
+        private static AnalyzeResponse GetAnalyzeResponse() => new AnalyzeResponse
+        {
+            SubmissionId = Guid.NewGuid().ToString(),
+            CatalogLastUpdated = DateTime.Now,
+            MissingDependencies = new List<MemberInfo>
+            {
+                new MemberInfo { MemberDocId = "MissingType1.Member()", DefinedInAssemblyIdentity = "Assembly1", TypeDocId = "MissingType1" },
+            },
+            Targets = new List<FrameworkName>
+            {
+                new FrameworkName("target1", Version.Parse("1.0.0.0"), "profile"),
+            },
+            UnresolvedUserAssemblies = new List<string> { "UnresolvedAssembly" },
+            BreakingChangeSkippedAssemblies = new List<AssemblyInfo>
+            {
+                new AssemblyInfo
+                {
+                    AssemblyIdentity = "breaking change skipped assembly",
+                    FileVersion = "42.42.42",
+                    IsExplicitlySpecified = true,
+                    Location = "C:/",
+                    TargetFrameworkMoniker = "tfm"
+                }
+            },
+            BreakingChanges = new List<BreakingChangeDependency>
+            {
+                new BreakingChangeDependency
+                {
+                    Break = new BreakingChange
+                    {
+                        ApplicableApis = new[] { "all of them" },
+                        Categories = new[] { "categories" },
+                        Details = "details",
+                        Id = "42",
+                        VersionBroken = Version.Parse("1.0"),
+                        VersionFixed = Version.Parse("1.1"),
+                        ImpactScope = BreakingChangeImpact.Edge
+                    },
+                    DependantAssembly = new AssemblyInfo
+                    {
+                        AssemblyIdentity = "DependentAssembly",
+                        FileVersion = Version.Parse("42.42.42").ToString(),
+                        IsExplicitlySpecified = true,
+                        Location = "c:/foo/bar",
+                        TargetFrameworkMoniker = "TFM"
+                    },
+                    Member = new MemberInfo
+                    {
+                        DefinedInAssemblyIdentity = "DependentAssembly",
+                        IsSupportedAcrossTargets = false,
+                        MemberDocId = "M:Foo.NotSupported",
+                        RecommendedChanges = "don't use this",
+                        TypeDocId = "T:Foo"
+                    }
+                }
+            }
+        };
     }
 }
