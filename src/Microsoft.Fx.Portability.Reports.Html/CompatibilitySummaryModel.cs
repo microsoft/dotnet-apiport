b27f2ae9da6dfb5e9738ab686c6a79510073afec
Charles Lowell <chlowe@microsoft.com>
2018-01-04 12:38:45 -0800

    Update HTML report generation (#538)
    * Refactor views
    * Render all sections of HTML report in test
    * Add resource strings
    * Increase text contrast
    * Add alt text to buttons and target support cells
    * Update RazorEngine dependency to 4.5.1-alpha002

---
+++ src/Microsoft.Fx.Portability.Reports.Html/CompatibilitySummaryModel.cs
@@ -0,0 +1,23 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Microsoft.Fx.Portability.ObjectModel;
+using Microsoft.Fx.Portability.Reporting.ObjectModel;
+using System.Collections.Generic;
+
+namespace Microsoft.Fx.Portability.Reports.Html
+{
+    public class CompatibilitySummaryModel
+    {
+        public IDictionary<BreakingChange, IEnumerable<MemberInfo>> Breaks { get; }
+        public AssemblyInfo Assembly { get; }
+        public ReportingResult ReportingResult { get; }
+
+        public CompatibilitySummaryModel(IDictionary<BreakingChange, IEnumerable<MemberInfo>> breaks, AssemblyInfo assembly, ReportingResult reportingResult)
+        {
+            Breaks = breaks;
+            Assembly = assembly;
+            ReportingResult = reportingResult;
+        }
+    }
+}
