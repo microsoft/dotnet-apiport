0cf44b9370b60592b1c38c0455d98952e15a6e4f
Connie Yau <conniey@microsoft.com>
2018-01-15 09:17:51 -0800

    Adding localizable Category, DisplayName and Description attributes

---
+++ src/ApiPort.VisualStudio.Common/Attributes/LocalizedDisplayNameAttribute.cs
@@ -0,0 +1,19 @@
+﻿using ApiPortVS.Resources;
+using System;
+using System.ComponentModel;
+
+namespace ApiPortVS.Attributes
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method | AttributeTargets.Property | AttributeTargets.Event)]
+    public class LocalizedDisplayNameAttribute : DisplayNameAttribute
+    {
+        /// <summary>
+        /// Creates a display name for the decorated member
+        /// </summary>
+        /// <param name="resourceName">Name of the resource in <see cref="LocalizedStrings"/></param>
+        public LocalizedDisplayNameAttribute(string resourceName)
+            : base(LocalizedStrings.ResourceManager.GetString(resourceName, LocalizedStrings.Culture))
+        {
+        }
+    }
+}
