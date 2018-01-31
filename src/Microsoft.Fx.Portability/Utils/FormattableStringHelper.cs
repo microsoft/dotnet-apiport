30fbba1536cf43752a6e73ba2bb07964cd7a8b33
Connie Yau <conniey@microsoft.com>
2018-01-31 10:14:48 -0800

    Fixing more FxCop issues (#569)
    * For each FormattableString, specify a culture or invariant
    
    * Suppress CA3053 because of the false positives

---
+++ src/Microsoft.Fx.Portability/Utils/FormattableStringHelper.cs
@@ -0,0 +1,13 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using System.Globalization;
+
+namespace Microsoft.Fx.Portability.Utils
+{
+    public static class FormattableStringHelper
+    {
+        public static string ToCurrentCulture(FormattableString formattableString) => formattableString.ToString(CultureInfo.CurrentCulture);
+    }
+}
