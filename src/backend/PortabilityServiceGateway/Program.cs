a1caa2ad2b3de6cdd8470e9cde81f839610f57ce
Mike Rousos <MikeRou@Microsoft.com>
2018-03-02 16:05:40 -0500

    Add logging

--- src/backend/PortabilityServiceGateway/Program.cs
+++ src/backend/PortabilityServiceGateway/Program.cs
@@ -1,20 +1,17 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using Serilog;
 
 namespace PortabilityServiceGateway
 {
     public class Program
     {
+        const string APP_INSIGHTS_KEY_KEY = "ApplicationInsights:InstrumentationKey";
+
         public static void Main(string[] args)
         {
             BuildWebHost(args).Run();
@@ -22,7 +19,35 @@ namespace PortabilityServiceGateway
 
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseApplicationInsights()
+                .ConfigureLogging(ConfigureLogging)
                 .UseStartup<Startup>()
                 .Build();
+
+        /// <summary>
+        /// Configure logging providers
+        /// </summary>
+        private static void ConfigureLogging(WebHostBuilderContext context, ILoggingBuilder loggingBuilder)
+        {
+            var env = context.HostingEnvironment;
+            var config = context.Configuration;
+
+            // Clear default providers
+            loggingBuilder.ClearProviders();
+
+            // Create Serilog configuration from config
+            var serilogLoggerConfiguration = new LoggerConfiguration()
+                .ReadFrom.Configuration(config);
+            
+            if (!env.IsDevelopment())
+            {
+                // In non-dev environments, add an App Insights sink
+                serilogLoggerConfiguration = serilogLoggerConfiguration
+                                                .WriteTo.ApplicationInsightsTraces(config[APP_INSIGHTS_KEY_KEY]);
+            }
+
+            // Add the Serilog logging provider
+            loggingBuilder.AddSerilog(serilogLoggerConfiguration.CreateLogger());
+        }
     }
 }
--- src/backend/PortabilityServiceGateway/Startup.cs
+++ src/backend/PortabilityServiceGateway/Startup.cs
@@ -1,16 +1,10 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
 
 namespace PortabilityServiceGateway
 {
