694799594cb021e1e2feb9e224a1133982cb1bd5
Charles Lowell <chlowe@microsoft.com>
2018-03-08 10:30:07 -0800

    Add Analyze function and tests (#589)
    * add Analyze function and tests
    * deserialize requests from streams

---
+++ src/backend/Functions/Analyze.cs
@@ -0,0 +1,52 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Fx.Portability;
+using Microsoft.Fx.Portability.ObjectModel;
+using System;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Functions
+{
+    public static class Analyze
+    {
+        [FunctionName("analyze")]
+        public static async Task<HttpResponseMessage> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "post", Route = null)] HttpRequestMessage req,
+            TraceWriter log)
+        {
+            var analyzeRequest = await DeserializeRequest(req.Content);
+            if (analyzeRequest == null)
+            {
+                log.Error("invalid request");
+                return req.CreateResponse(HttpStatusCode.BadRequest);
+            }
+
+            var submissionId = Guid.NewGuid().ToString();
+            log.Info($"created submission id {submissionId}");
+
+            var response = req.CreateResponse(HttpStatusCode.OK);
+            response.Content = new StringContent(submissionId);
+
+            return response;
+        }
+
+        public static async Task<AnalyzeRequest> DeserializeRequest(HttpContent content)
+        {
+            try
+            {
+                var stream = await content.ReadAsStreamAsync();
+                return DataExtensions.DecompressToObject<AnalyzeRequest>(stream);
+            }
+            catch
+            {
+                return null;
+            }
+        }
+    }
+}
---
+++ tests/backend/Functions.Tests/AnalyzeTests.cs
@@ -0,0 +1,70 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Microsoft.Azure.WebJobs.Host;
+using System;
+using System.Diagnostics;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using Xunit;
+
+namespace Functions.Tests
+{
+    public class AnalyzeTests
+    {
+        [Fact]
+        public static async Task ReturnsBadRequestForMalformedContent()
+        {
+            var request = PostFromConsoleApiPort;
+            request.Content = new StringContent("{ \"json\": \"json\" }");
+
+            var response = await Analyze.Run(request, new DoNothingTraceWriter());
+
+            Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
+        }
+
+        [Fact]
+        public static async Task ReturnsGuidForCompressedAnalyzeRequest()
+        {
+            var gzippedAnalyzeRequest = typeof(AnalyzeTests).Assembly
+                .GetManifestResourceStream("Functions.Tests.Resources.apiport.exe.AnalyzeRequest.json.gz");
+
+            var request = PostFromConsoleApiPort;
+            request.SetConfiguration(new HttpConfiguration());
+            request.Content = new StreamContent(gzippedAnalyzeRequest);
+            request.Content.Headers.Add("Content-Encoding", "gzip");
+            request.Content.Headers.Add("Content-Type", "application/json");
+
+            var response = await Analyze.Run(request, new DoNothingTraceWriter());
+            var body = await response.Content.ReadAsStringAsync();
+
+            Assert.True(Guid.TryParse(body, out var _));
+        }
+
+        private static HttpRequestMessage PostFromConsoleApiPort
+        {
+            get
+            {
+                var req = new HttpRequestMessage(HttpMethod.Post, "");
+                req.Headers.Add("Accept", "application/json");
+                req.Headers.Add("Accept-Encoding", "gzip, deflate");
+                req.Headers.Add("Client-Type", "ApiPort_Console");
+                req.Headers.Add("Client-Version", "2.4.0.2");
+                req.Headers.Add("Expect", "100-continue");
+
+                return req;
+            }
+        }
+
+        private class DoNothingTraceWriter : TraceWriter
+        {
+            public DoNothingTraceWriter() : base(TraceLevel.Off)
+            { }
+
+            public override void Trace(TraceEvent traceEvent)
+            { }
+        }
+    }
+}
