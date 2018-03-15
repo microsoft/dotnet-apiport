a554a3316c95de1ae6046ca3a75bcdbc81a99081
Mike Rousos <MikeRou@Microsoft.com>
2018-03-15 15:12:43 -0400

    Add new test project for API gateway

---
+++ tests/backend/PortabilityService.Gateway.Tests/HealthCheckTests.cs
@@ -0,0 +1,17 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using Xunit;
+
+namespace PortabilityService.Gateway.Tests
+{
+    public class HealthCheckTests
+    {
+        [Fact]
+        public static void HealthCheckTest()
+        {
+
+        }
+    }
+}
