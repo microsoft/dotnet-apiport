d8b3a7572fcd3efd638db2fbfa53c41a5346dbb2
Jeremy Meng <yumeng@microsoft.com>
2018-03-27 13:25:13 -0700

    Update to use injected IStorage for saving analyze requests

--- tests/backend/Functions.Tests/AnalyzeTests.cs
+++ tests/backend/Functions.Tests/AnalyzeTests.cs
@@ -1,16 +1,20 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging.Abstractions;
+using Microsoft.Fx.Portability;
+using Microsoft.Fx.Portability.ObjectModel;
+using NSubstitute;
 using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
 using System.Web.Http;
-using Xunit;
 using WorkflowManagement;
-using Microsoft.Extensions.Logging.Abstractions;
-using NSubstitute;
-using Microsoft.Azure.WebJobs;
+using Xunit;
 
 namespace Functions.Tests
 {
@@ -22,7 +26,8 @@ namespace Functions.Tests
             var request = PostFromConsoleApiPort;
             request.Content = new StringContent("{ \"json\": \"json\" }");
 
-            var response = await Analyze.Run(request, Substitute.For<ICollector<WorkflowQueueMessage>>(), NullLogger.Instance);
+            var storage = Substitute.For<IStorage>();
+            var response = await Analyze.Run(request, Substitute.For<ICollector<WorkflowQueueMessage>>(), storage, NullLogger.Instance);
 
             Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
         }
@@ -41,12 +46,38 @@ namespace Functions.Tests
 
             WorkflowManager.Initialize();
             var workflowQueue = Substitute.For<ICollector<WorkflowQueueMessage>>();
-            var response = await Analyze.Run(request, workflowQueue, NullLogger.Instance);
+            var storage = Substitute.For<IStorage>();
+            storage.SaveToBlobAsync(Arg.Any<AnalyzeRequest>(), Arg.Any<string>()).Returns(Task.FromResult(true));
+            var response = await Analyze.Run(request, workflowQueue, storage, NullLogger.Instance);
             var body = await response.Content.ReadAsStringAsync();
 
             Assert.True(Guid.TryParse(body, out var submissionId));
 
             workflowQueue.Received().Add(Arg.Is<WorkflowQueueMessage>(x => x.SubmissionId == submissionId.ToString() && x.Stage == WorkflowStage.Analyze));
+        }
+
+        [Fact]
+        public static async Task SavedRequestMatchesOriginal()
+        {
+            var gzippedAnalyzeRequestStream = typeof(AnalyzeTests).Assembly
+                .GetManifestResourceStream("Functions.Tests.Resources.apiport.exe.AnalyzeRequest.json.gz");
+
+            var expectedStream = new MemoryStream();
+            gzippedAnalyzeRequestStream.CopyTo(expectedStream);
+            gzippedAnalyzeRequestStream.Seek(0, SeekOrigin.Begin);
+
+            var request = PostFromConsoleApiPort;
+            request.SetConfiguration(new HttpConfiguration());
+            request.Content = new StreamContent(gzippedAnalyzeRequestStream);
+            request.Content.Headers.Add("Content-Encoding", "gzip");
+            request.Content.Headers.Add("Content-Type", "application/json");
+
+            WorkflowManager.Initialize();
+            var workflowQueue = Substitute.For<ICollector<WorkflowQueueMessage>>();
+            var storage = new TestStorage();
+            var response = await Analyze.Run(request, workflowQueue, storage, NullLogger.Instance);
+
+            Assert.Equal(expectedStream.ToArray(), storage.Stored);
         }
 
         private static HttpRequestMessage PostFromConsoleApiPort
@@ -61,6 +92,42 @@ namespace Functions.Tests
                 req.Headers.Add("Expect", "100-continue");
 
                 return req;
+            }
+        }
+
+        private class TestStorage : IStorage
+        {
+            public byte[] Stored { get; set; }
+
+            public Task AddJobToQueueAsync(string submissionId)
+            {
+                throw new NotImplementedException();
+            }
+
+            public IEnumerable<ProjectSubmission> GetProjectSubmissions()
+            {
+                throw new NotImplementedException();
+            }
+
+            public Task<IEnumerable<UsageData>> GetUsageDataAsync()
+            {
+                throw new NotImplementedException();
+            }
+
+            public Task<AnalyzeRequest> RetrieveRequestAsync(string uniqueId)
+            {
+                throw new NotImplementedException();
+            }
+
+            public Task<IEnumerable<string>> RetrieveSubmissionIdsAsync()
+            {
+                throw new NotImplementedException();
+            }
+
+            public Task<bool> SaveToBlobAsync(AnalyzeRequest analyzeRequest, string submissionId)
+            {
+                Stored = analyzeRequest.SerializeAndCompress();
+                return Task.FromResult(true);
             }
         }
     }
