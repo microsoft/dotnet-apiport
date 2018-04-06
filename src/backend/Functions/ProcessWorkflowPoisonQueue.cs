d782b52d126a86374d57fa6fe70d2602365370ed
unknown <nicolehaugen79@hotmail.com>
2018-04-06 17:10:53 +0000

    removed unneeded using

--- src/backend/Functions/ProcessWorkflowPoisonQueue.cs
+++ src/backend/Functions/ProcessWorkflowPoisonQueue.cs
@@ -1,9 +1,7 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-using System;
 using Microsoft.Azure.WebJobs;
-using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Extensions.Logging;
 using WorkflowManagement;
 
