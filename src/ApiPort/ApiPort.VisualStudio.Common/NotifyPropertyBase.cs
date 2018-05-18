f1727c1a360dc201c3b44490b46a5007749c7354
Charles Lowell <chlowe@microsoft.com>
2018-05-18 13:00:44 -0700

    ensure UI objects are created on the correct thread

--- src/ApiPort/ApiPort.VisualStudio.Common/NotifyPropertyBase.cs
+++ src/ApiPort/ApiPort.VisualStudio.Common/NotifyPropertyBase.cs
@@ -1,22 +1,23 @@
 ﻿// Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
 using System.Threading;
-using System.Windows.Threading;
 
 namespace ApiPortVS
 {
     public class NotifyPropertyBase : INotifyPropertyChanged
     {
         public event PropertyChangedEventHandler PropertyChanged;
-        private readonly SynchronizationContext _context;
+        protected SynchronizationContext Context { get; }
 
         public NotifyPropertyBase()
         {
-            _context = new DispatcherSynchronizationContext(Dispatcher.CurrentDispatcher);
+            // SynchronizationContext.Current will be null if we're not on the UI thread
+            Context = SynchronizationContext.Current ?? throw new ArgumentNullException("SynchronizationContext.Current");
         }
 
         protected void UpdateProperty<T>(ref T field, T value, [CallerMemberName] string propertyName = "")
@@ -37,13 +38,13 @@ namespace ApiPortVS
                 return;
             }
 
-            if (_context == SynchronizationContext.Current)
+            if (Context == SynchronizationContext.Current)
             {
                 propertyChanged.Invoke(this, new PropertyChangedEventArgs(propertyName));
             }
             else
             {
-                _context.Post(_ => propertyChanged.Invoke(this, new PropertyChangedEventArgs(propertyName)), null);
+                Context.Post(_ => propertyChanged.Invoke(this, new PropertyChangedEventArgs(propertyName)), null);
             }
         }
     }
