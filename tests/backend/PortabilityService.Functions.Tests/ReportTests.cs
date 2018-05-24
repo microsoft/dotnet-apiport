56e002111a832a34cccc61b49cd3b6137ca5a779
Charles Lowell <chlowe@microsoft.com>
2018-05-24 14:41:09 -0700

    add tests

---
+++ tests/backend/PortabilityService.Functions.Tests/ReportTests.cs
@@ -0,0 +1,84 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Microsoft.Extensions.Logging;
+using Microsoft.Fx.Portability.Azure;
+using Microsoft.Fx.Portability.ObjectModel;
+using NSubstitute;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace PortabilityService.Functions.Tests
+{
+    public class ReportTests
+    {
+        [Fact]
+        public async Task ReturnsUnauthorizedForInvalidToken()
+        {
+            var storage = Substitute.For<IStorage>();
+            storage.RetrieveRequestAsync(Arg.Any<string>()).Returns(new AnalyzeRequest());
+
+            var tokenValidator = Substitute.For<IReportTokenValidator>();
+            tokenValidator.RequestHasValidToken(Arg.Any<HttpRequestMessage>()).Returns(false);
+
+            var request = new HttpRequestMessage(HttpMethod.Get, "");
+
+            var response = await Report.Run(request, "submissionId", storage, tokenValidator, Substitute.For<ILogger>());
+
+            Assert.Equal(HttpStatusCode.Unauthorized, response.StatusCode);
+        }
+
+        [Fact]
+        public async Task ReturnsAcceptedWhenAnalysisNotComplete()
+        {
+            var storage = Substitute.For<IStorage>();
+            storage.RetrieveRequestAsync(Arg.Any<string>()).Returns(new AnalyzeRequest());
+
+            var tokenValidator = Substitute.For<IReportTokenValidator>();
+            tokenValidator.RequestHasValidToken(Arg.Any<HttpRequestMessage>()).Returns(true);
+
+            var request = new HttpRequestMessage(HttpMethod.Get, "");
+
+            var response = await Report.Run(request, "submissionId", storage, tokenValidator, Substitute.For<ILogger>());
+
+            Assert.Equal(HttpStatusCode.Accepted, response.StatusCode);
+        }
+
+        [Fact]
+        public async Task AcceptedResponseIncludesRetryAfter()
+        {
+            var storage = Substitute.For<IStorage>();
+            storage.RetrieveRequestAsync(Arg.Any<string>()).Returns(new AnalyzeRequest());
+
+            var tokenValidator = Substitute.For<IReportTokenValidator>();
+            tokenValidator.RequestHasValidToken(Arg.Any<HttpRequestMessage>()).Returns(true);
+
+            var request = new HttpRequestMessage(HttpMethod.Get, "");
+
+            var response = await Report.Run(request, "submissionId", storage, tokenValidator, Substitute.For<ILogger>());
+
+            Assert.NotNull(response.Headers.RetryAfter.Delta);
+        }
+
+        [Fact]
+        public async Task ReturnsUnsupportedMediaTypeWhenNoReportWriterFound()
+        {
+            var storage = Substitute.For<IStorage>();
+            storage.RetrieveRequestAsync(Arg.Any<string>()).Returns(new AnalyzeRequest());
+            storage.RetrieveResultFromBlobAsync(Arg.Any<string>()).Returns(new AnalyzeResult());
+
+            var tokenValidator = Substitute.For<IReportTokenValidator>();
+            tokenValidator.RequestHasValidToken(Arg.Any<HttpRequestMessage>()).Returns(true);
+
+            var request = new HttpRequestMessage(HttpMethod.Get, "");
+            request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("unknown/mediatype"));
+
+            var response = await Report.Run(request, "submissionId", storage, tokenValidator, Substitute.For<ILogger>());
+
+            Assert.Equal(HttpStatusCode.UnsupportedMediaType, response.StatusCode);
+        }
+    }
+}
