badd92aae3681255bd685881af85166c39d88c58
Christopher Roberts <chrisrt@microsoft.com>
2018-06-18 13:36:35 -0700

    Added smoke test to unit tests for Targets

---
+++ tests/backend/PortabilityService.Functions.Tests/TargetsTests.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Extensions.Logging;
+using Microsoft.Fx.Portability.Azure;
+using NSubstitute;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace PortabilityService.Functions.Tests
+{
+    public class TargetsTests
+    {
+        /// <summary>
+        /// Smoke test - validates that embedded resource can be fetched and a value is returned from the endpoint
+        /// </summary>
+        [Fact]
+        public async Task FetchesTargets()
+        {
+            var tokenValidator = Substitute.For<IReportTokenValidator>();
+            tokenValidator.RequestHasValidToken(Arg.Any<HttpRequestMessage>()).Returns(false);
+
+            using (var request = new HttpRequestMessage(HttpMethod.Get, string.Empty))
+            {
+                var response = await Targets.Run(request, Substitute.For<ILogger>());
+                Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+                var contents = await response.Content.ReadAsStringAsync();
+#pragma warning disable CA1307 // Specify StringComparison (this string is static at this time)
+                Assert.Contains(".NET Core + Platform Extensions", contents);
+#pragma warning restore CA1307 // Specify StringComparison
+            }
+        }
+    }
+}
