b0523e30592eebd200d69acf86407718fe0ee184
Chris Roberts <chrisrt@microsoft.com>
2018-06-19 11:34:42 -0700

    Added function to expose available targets (#680)
    * Added function to expose targets
    
    * Removed un-necessary async/await in helper method
    
    * Added smoke test to unit tests for Targets
    
    * Updated unit test - ordinal comparison
    
    * Added missing license header to TargetsTests

---
+++ tests/backend/PortabilityService.Functions.Tests/TargetsTests.cs
@@ -0,0 +1,36 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Microsoft.Extensions.Logging;
+using Microsoft.Fx.Portability.Azure;
+using NSubstitute;
+using System;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace PortabilityService.Functions.Tests
+{
+    public class TargetsTests
+    {
+        /// <summary>
+        /// Smoke test - validates that embedded resource can be fetched and a value is returned from the endpoint
+        /// </summary>
+        [Fact]
+        public async Task FetchesTargets()
+        {
+            var tokenValidator = Substitute.For<IReportTokenValidator>();
+            tokenValidator.RequestHasValidToken(Arg.Any<HttpRequestMessage>()).Returns(false);
+
+            using (var request = new HttpRequestMessage(HttpMethod.Get, string.Empty))
+            {
+                var response = await Targets.Run(request, Substitute.For<ILogger>());
+                Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+                var contents = await response.Content.ReadAsStringAsync();
+
+                Assert.Contains(".NET Core + Platform Extensions", contents, StringComparison.Ordinal);
+            }
+        }
+    }
+}
