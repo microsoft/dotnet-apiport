6035995b7e857a1230eb092b5dbb54811503dd33
Charles Lowell <chlowe@microsoft.com>
2018-06-19 14:00:29 -0700

    add readiness check

---
+++ src/backend/PortabilityService.AnalysisService/Controllers/ReadyController.cs
@@ -0,0 +1,36 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Fx.Portability;
+using Microsoft.Fx.Portability.Cache;
+using System;
+using System.Linq;
+
+namespace PortabilityService.AnalysisService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReadyController : ControllerBase
+    {
+        private readonly ApiPortData _apiPortData;
+        private readonly IObjectCache<CatalogIndex> _catalog;
+
+        public ReadyController(ApiPortData apiPortData, IObjectCache<CatalogIndex> catalog)
+        {
+            _apiPortData = apiPortData;
+            _catalog = catalog;
+        }
+
+        [HttpGet]
+        public IActionResult Ready() =>
+            CatalogReady() && GitHubDataReady() ? Ok() : new StatusCodeResult(StatusCodes.Status503ServiceUnavailable);
+
+        private bool CatalogReady() =>
+            _catalog?.LastUpdated != default(DateTimeOffset) && _catalog.Value?.Catalog?.DocIds?.Count() > 0;
+
+        private bool GitHubDataReady() =>
+            _apiPortData?.BreakingChangesDictionary?.Keys.Count() > 0 && _apiPortData?.RecommendedChanges?.Count() > 0;
+    }
+}
---
+++ tests/backend/PortabilityService.AnalysisService.Tests/ReadyControllerTests.cs
@@ -0,0 +1,112 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Fx.Portability;
+using Microsoft.Fx.Portability.Cache;
+using Microsoft.Fx.Portability.ObjectModel;
+using NSubstitute;
+using PortabilityService.AnalysisService.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace PortabilityService.AnalysisService.Tests
+{
+    public class ReadyControllerTests
+    {
+        [Fact]
+        public void ReadyWhenCatalogAndGitHubDataReady()
+        {
+            var apiPortData = new ApiPortData
+            {
+                BreakingChangesDictionary = new Dictionary<string, ICollection<BreakingChange>>
+                {
+                    ["foo"] = new List<BreakingChange>(new[] { new BreakingChange() })
+                },
+                RecommendedChanges = new Dictionary<string, string> { ["a"] = "b" }
+            };
+
+            var catalogCache = TestCatalogCache();
+            var controller = new ReadyController(apiPortData, catalogCache);
+
+            var response = controller.Ready();
+
+            Assert.IsType<OkResult>(response);
+        }
+
+        [Fact]
+        public void NotReadyWhenCatalogNotReady()
+        {
+            var apiPortData = new ApiPortData
+            {
+                BreakingChangesDictionary = new Dictionary<string, ICollection<BreakingChange>>
+                {
+                    ["foo"] = new List<BreakingChange>(new[] { new BreakingChange() })
+                },
+                RecommendedChanges = new Dictionary<string, string> { ["a"] = "b" }
+            };
+            var catalogCache = TestCatalogCache(true);
+            var controller = new ReadyController(apiPortData, catalogCache);
+
+            var response = controller.Ready();
+
+            Assert.IsType<StatusCodeResult>(response);
+            Assert.Equal(StatusCodes.Status503ServiceUnavailable, (response as StatusCodeResult).StatusCode);
+        }
+
+        [Fact]
+        public void NotReadyWhenNoBreakingChanges()
+        {
+            var apiPortData = new ApiPortData
+            {
+                BreakingChangesDictionary = new Dictionary<string, ICollection<BreakingChange>>(),
+                RecommendedChanges = new Dictionary<string, string> { ["a"] = "b" }
+            };
+
+            var catalogCache = TestCatalogCache();
+            var controller = new ReadyController(apiPortData, catalogCache);
+
+            var response = controller.Ready();
+
+            Assert.IsType<StatusCodeResult>(response);
+            Assert.Equal(StatusCodes.Status503ServiceUnavailable, (response as StatusCodeResult).StatusCode);
+        }
+
+        [Fact]
+        public void NotReadyWhenNoRecommendedChanges()
+        {
+            var apiPortData = new ApiPortData
+            {
+                BreakingChangesDictionary = new Dictionary<string, ICollection<BreakingChange>>
+                {
+                    ["foo"] = new List<BreakingChange>(new[] { new BreakingChange() })
+                },
+                RecommendedChanges = new Dictionary<string, string>()
+            };
+
+            var catalogCache = TestCatalogCache();
+            var controller = new ReadyController(apiPortData, catalogCache);
+
+            var response = controller.Ready();
+
+            Assert.IsType<StatusCodeResult>(response);
+            Assert.Equal(StatusCodes.Status503ServiceUnavailable, (response as StatusCodeResult).StatusCode);
+        }
+
+        private static IObjectCache<CatalogIndex> TestCatalogCache(bool empty = false)
+        {
+            var catalog = Substitute.For<IApiCatalogLookup>();
+            var docIds = empty ? Enumerable.Empty<string>() : new[] { "T:a.doc.id", "T:a.nother.doc.id" };
+            catalog.DocIds.Returns(docIds);
+            var catalogIndex = new CatalogIndex(catalog, Substitute.For<ISearcher<string>>());
+            var catalogCache = Substitute.For<IObjectCache<CatalogIndex>>();
+            catalogCache.Value.Returns(catalogIndex);
+            catalogCache.LastUpdated.Returns(DateTimeOffset.Now);
+
+            return catalogCache;
+        }
+    }
+}
