c9ac5aaad2a7a092e42e43284ca90afbd11e892a
Connie Yau <conniey@microsoft.com>
2018-09-08 17:10:32 -0700

    Fixing some more SA1516 warnings

--- src/lib/Microsoft.Fx.Portability/ObjectModel/ApiInfoStorage.cs
+++ src/lib/Microsoft.Fx.Portability/ObjectModel/ApiInfoStorage.cs
@@ -10,15 +10,20 @@ namespace Microsoft.Fx.Portability.ObjectModel
     public class ApiInfoStorage
     {
         public string DocId { get; set; }
+
         public string Type { get; set; }
+
         public string Name { get; set; }
+
         public string FullName { get; set; }
 
         /// <summary>
         /// DocId of the parent. Null if the Api does not have a parent.
         /// </summary>
         public string Parent { get; set; }
+
         public IReadOnlyCollection<FrameworkName> Targets { get; set; }
+
         public IReadOnlyCollection<ApiMetadataStorage> Metadata { get; set; }
     }
 }
--- src/lib/Microsoft.Fx.Portability/ObjectModel/ApiMetadataStorage.cs
+++ src/lib/Microsoft.Fx.Portability/ObjectModel/ApiMetadataStorage.cs
@@ -8,6 +8,7 @@ namespace Microsoft.Fx.Portability.ObjectModel
     public class ApiMetadataStorage
     {
         public string MetadataKey { get; set; }
+
         public string Value { get; set; }
     }
 }
