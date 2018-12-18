9bb1024624d81bf66a0378351797bbd357925dad
Taylor Southwick <tasou@microsoft.com>
2018-12-18 12:00:22 -0800

    Move HTML report generator to a razor library (#742)

---
+++ src/lib/Microsoft.Fx.Portability.Reports.Html/HtmlHelper.cs
@@ -0,0 +1,52 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.Fx.Portability.Reports.Html;
+using Microsoft.Fx.Portability.Reports.Html.Resources;
+
+using static System.FormattableString;
+
+namespace Microsoft.Fx.Portability.Reports
+{
+    public static class HtmlHelper
+    {
+        public static IHtmlContent ConvertMarkdownToHtml<T>(this IHtmlHelper<T> helper, string markdown)
+        {
+            return helper.Raw(CommonMark.CommonMarkConverter.Convert(markdown));
+        }
+
+        public static IHtmlContent Raw<T>(this IHtmlHelper<T> helper, string rawString)
+        {
+            return new HtmlString(rawString);
+        }
+
+        public static IHtmlContent TargetSupportCell<T>(this IHtmlHelper<T> helper, TargetSupportedIn supportStatus)
+        {
+            var supported = supportStatus.SupportedIn != null
+                         && supportStatus.Target.Version >= supportStatus.SupportedIn;
+
+            var imageId = supported ? "icon-supported" : "icon-unsupported";
+            var title = supported ? LocalizedStrings.Supported : LocalizedStrings.NotSupported;
+            var icon = $"<svg class=\"support-icon\"><use href=#{imageId}></use></svg>";
+
+            return helper.Raw(Invariant($"<td class=\"textCentered\" title=\"{title}\">{icon}</td>"));
+        }
+
+        public static IHtmlContent BreakingChangeCountCell<T>(this IHtmlHelper<T> helper, int breaks, int warningThreshold, int errorThreshold)
+        {
+            var className = string.Empty;
+            if (breaks <= warningThreshold)
+            {
+                className = "NoBreakingChanges";
+            }
+            else
+            {
+                className = breaks <= errorThreshold ? "FewBreakingChanges" : "ManyBreakingChanges";
+            }
+
+            return helper.Raw(Invariant($"<td class=\"textCentered {className}\">{breaks}</td>"));
+        }
+    }
+}
---
+++ src/lib/Microsoft.Fx.Portability.Reports.Html/HtmlRazorReportWriter.cs
@@ -0,0 +1,82 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Hosting.Internal;
+using Microsoft.AspNetCore.Mvc.Razor;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
+using Microsoft.Extensions.ObjectPool;
+using Microsoft.Fx.Portability.ObjectModel;
+using Microsoft.Fx.Portability.Reporting;
+using System;
+using System.IO;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace Microsoft.Fx.Portability.Reports
+{
+    public sealed class HtmlRazorReportWriter : IReportWriter, IDisposable
+    {
+        private static readonly ResultFormatInformation _formatInformation = new ResultFormatInformation
+        {
+            DisplayName = "HTML",
+            MimeType = "text/html",
+            FileExtension = ".html"
+        };
+
+        private readonly ITargetMapper _targetMapper;
+        private readonly ServiceProvider _serviceProvider;
+        private readonly IServiceScopeFactory _factory;
+
+        public HtmlRazorReportWriter(ITargetMapper targetMapper)
+        {
+            _targetMapper = targetMapper;
+            _serviceProvider = BuildProvider();
+            _factory = _serviceProvider.GetRequiredService<IServiceScopeFactory>();
+        }
+
+        public ResultFormatInformation Format => _formatInformation;
+
+        public async Task WriteStreamAsync(Stream stream, AnalyzeResponse response)
+        {
+            using (var scope = _factory.CreateScope())
+            {
+                var model = new RazorHtmlObject(response, _targetMapper);
+                var helper = scope.ServiceProvider.GetRequiredService<RazorViewToStringRenderer>();
+
+                await helper.RenderViewAsync("Views/ReportTemplate.cshtml", model, stream);
+            }
+        }
+
+        private static ServiceProvider BuildProvider()
+        {
+            var services = new ServiceCollection();
+            var fileProvider = new PhysicalFileProvider(Directory.GetCurrentDirectory());
+
+            services.AddSingleton<IHostingEnvironment>(new HostingEnvironment
+            {
+                ApplicationName = Assembly.GetEntryAssembly().GetName().Name,
+                WebRootFileProvider = fileProvider
+            });
+
+            services.Configure<RazorViewEngineOptions>(options =>
+            {
+                options.FileProviders.Clear();
+                options.FileProviders.Add(fileProvider);
+            });
+
+            services.AddSingleton<ObjectPoolProvider, DefaultObjectPoolProvider>();
+            services.AddLogging();
+            services.AddMvc();
+            services.AddTransient<RazorViewToStringRenderer>();
+
+            return services.BuildServiceProvider();
+        }
+
+        public void Dispose()
+        {
+            _serviceProvider.Dispose();
+        }
+    }
+}
