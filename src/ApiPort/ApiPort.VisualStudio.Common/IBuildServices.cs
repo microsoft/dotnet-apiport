85679bd43f56ee5fdd9f9a4d1923cf3c8b283a99
Taylor Southwick <tasou@microsoft.com>
2018-12-17 23:44:00 -0800

    Add VS2019 support (#739)
    * Use MEF to get 2017 specific items
    
    * Add support for VS 2019
    
    * Update targets

---
+++ src/ApiPort/ApiPort.VisualStudio.Common/IBuildServices.cs
@@ -0,0 +1,12 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Microsoft.VisualStudio.Shell.Interop;
+
+namespace ApiPortVS
+{
+    public interface IBuildServices
+    {
+        IVsSolutionBuildManager2 BuildManager { get; }
+    }
+}
