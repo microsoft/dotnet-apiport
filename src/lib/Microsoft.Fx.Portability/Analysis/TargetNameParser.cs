eac0f4bb2f49d7542923ba769cb5dd83fa01c780
Connie <conniey@microsoft.com>
2019-02-01 15:24:06 -0800

    Update documentation

--- src/lib/Microsoft.Fx.Portability/Analysis/TargetNameParser.cs
+++ src/lib/Microsoft.Fx.Portability/Analysis/TargetNameParser.cs
@@ -83,12 +83,13 @@ namespace Microsoft.Fx.Portability.Analysis
         /// Parse a string containing target names into FrameworkNames.
         ///
         /// Try the following in order:
-        ///   1. Check if the target specified uses the 'simple' name (i.e. Windows, .NET Framework) then get the latest version for it
-        ///   2. Try to parse it as a target name. If the target was not a valid FrameworkName, an ArgumentException will be thrown and passed down to user.
-        /// <exception cref="UnknownTargetException">Thrown when a target is unknown</exception>
+        ///   1. Check if the target specified uses a FrameworkName (ie. .NET Core, Version=1.0)
+        ///   2. Check if the target specified uses the 'simple' name (i.e. Windows, .NET Framework)
+        ///      then get the latest version for it.
         /// </summary>
         /// <param name="skipNonExistent">true to suppress <see cref="UnknownTargetException"/>
         /// when a target is not found. false, will not throw and skip that target instead.</param>
+        /// <exception cref="UnknownTargetException">Thrown when a target is unknown</exception>
         private ICollection<FrameworkName> ParseTargets(IEnumerable<string> targets, bool skipNonExistent)
         {
             bool TryParseFrameworkName(string targetName, out FrameworkName frameworkName)
