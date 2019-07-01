9e5c3d1f3c65817651af69934d35905262acde33
Katie Lee <ktjlee@umich.edu>
2019-07-01 11:15:33 -0700

    added new changes to exportresult and apianalyzer from old repository

--- src/ApiPort/ApiPort.GUI/PortAPIUI/ApiAnalyzer.cs
+++ src/ApiPort/ApiPort.GUI/PortAPIUI/ApiAnalyzer.cs
@@ -1,4 +1,5 @@
-﻿using Newtonsoft.Json.Linq;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -6,6 +7,7 @@ using System.IO;
 using System.Net;
 using System.Reflection;
 using System.Text;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Documents;
 using System.Windows.Threading;
@@ -14,16 +16,28 @@ namespace PortAPIUI
 {
     class ApiAnalyzer
     {
-        public static void AnalyzeAssemblies(List<string> assemblies)
+        public static JArray AnalyzeAssemblies(List<string> assemblies)
         {
             MessageBox.Show("Hi from Katie");
             string reportLocation = ExportResult.ExportApiResult("", ".json", true);
             string textFromFile = System.IO.File.ReadAllText(reportLocation);
-            JObject apiResult = JObject.Parse(textFromFile);
-            foreach (var target in apiResult["MissingDependencies"])
-            {
-                Debug.WriteLine(target);
-            }
+            dynamic dynobj = JsonConvert.DeserializeObject(textFromFile);
+
+            //will contain a list of dictionaries with the following following;
+            //{ 
+            //"DefinedInAssemblyIdentity": "PresentationFramework, Version=4.0.0.0, Culture=neutral, PublicKeyToken=31bf3856ad364e35",
+            //  "MemberDocId": "M:System.Windows.Shapes.Shape.set_Stroke(System.Windows.Media.Brush)",
+            //  "TypeDocId": "T:System.Windows.Shapes.Shape",
+            //  "RecommendedChanges": "",
+            //  "SourceCompatibleChange": "",
+            //  "TargetStatus": [
+            //    "3.0",
+            //    null
+            //  ]
+            //}
+            JArray apiList = dynobj.MissingDependencies;
+            return apiList;
+
 
         }
     }
--- src/ApiPort/ApiPort.GUI/PortAPIUI/ExportResult.cs
+++ src/ApiPort/ApiPort.GUI/PortAPIUI/ExportResult.cs
@@ -55,48 +55,49 @@ namespace PortAPIUI
             {
                 if (o.Data != null)
                 {
-                    Application.Current.Dispatcher.Invoke(() =>
-                    {
-                        msg.Add(o.Data);
-                    });
+                    //Application.Current.Dispatcher.Invoke(() =>
+                    //{
+                    //    msg.Add(o.Data);
+                    //});
                 }
                 //AnalzeBtn.IsEnabled = true;
             };
 
-            p.Exited += delegate
-            {
-                Application.Current.Dispatcher.Invoke(() =>
-                {
-                    string text;
+            //p.Exited += delegate
+            //{
+            //    Application.Current.Dispatcher.Invoke(() =>
+            //    {
+            //        string text;
+            //        if (msg.Count != 17) // Was not successful
+            //        {
 
-                    if (msg.Count != 17) // Was not successful
-                    {
+            //            if (msg.Count < 10) // Exception was thrown in the API console tool
+            //            {
+            //                text = $"Unable to analyze. The access to the specified path might be denied.";
+            //            }
+            //            else
+            //            {
+            //                text = msg.FindLast(o => !String.IsNullOrEmpty(o));
+            //                text = text.Trim(new char[] { '*', ' ' });
+            //                if (!text.Equals("No files were found to analyze.", StringComparison.InvariantCultureIgnoreCase))
+            //                {
+            //                    msg.RemoveRange(0, 10);
+            //                    var details = String.Join(Environment.NewLine, msg.ToArray());
+            //                    text = $"Unable to analyze.{Environment.NewLine}Details:{Environment.NewLine}{details}";
+            //                }
+            //            }
+            //        }
+            //        else // Was successful
+            //        {
+            //        }
+            //    });
 
-                        if (msg.Count < 10) // Exception was thrown in the API console tool
-                        {
-                            text = $"Unable to analyze. The access to the specified path might be denied.";
-                        }
-                        else
-                        {
-                            text = msg.FindLast(o => !String.IsNullOrEmpty(o));
-                            text = text.Trim(new char[] { '*', ' ' });
-                            if (!text.Equals("No files were found to analyze.", StringComparison.InvariantCultureIgnoreCase))
-                            {
-                                msg.RemoveRange(0, 10);
-                                var details = String.Join(Environment.NewLine, msg.ToArray());
-                                text = $"Unable to analyze.{Environment.NewLine}Details:{Environment.NewLine}{details}";
-                            }
-                        }
-                    }
-                    else // Was successful
-                    {
-                    }
+            //};
 
-                });
-
-            };
             p.Start();
-            p.BeginOutputReadLine();
+            //p.BeginOutputReadLine();
+            p.WaitForExit();
+
             return exportPath;
         }
         private static string GenerateReportPath(string fileExtension)
