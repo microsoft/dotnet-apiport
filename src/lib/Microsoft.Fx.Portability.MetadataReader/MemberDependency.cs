8f735262c9bccc9ca00d43a6d0f234f8237c5241
Alicia Li <alicial@microsoft.com>
2019-08-21 11:32:46 -0700

    fixes for CR feedback

--- src/lib/Microsoft.Fx.Portability.MetadataReader/MemberDependency.cs
+++ src/lib/Microsoft.Fx.Portability.MetadataReader/MemberDependency.cs
@@ -106,7 +106,7 @@ namespace Microsoft.Fx.Portability.Analyzer
         {
             if (!_hashComputed)
             {
-                _hashCode = ((DefinedInAssemblyIdentity?.ToString() ?? string.Empty) + (MemberDocId ?? string.Empty) + IsPrimitive.ToString((IFormatProvider)CultureInfo.CurrentUICulture)).GetHashCode() ^ CallingAssembly.GetHashCode();
+                _hashCode = ((DefinedInAssemblyIdentity?.ToString() ?? string.Empty) + (MemberDocId ?? string.Empty) + IsPrimitive.ToString((IFormatProvider)CultureInfo.InvariantCulture)).GetHashCode() ^ CallingAssembly.GetHashCode();
                 _hashComputed = true;
             }
 
