bb8692b7e4904ed9b88899585841253df0d5354a
Alicia Li <alicial@microsoft.com>
2019-10-15 17:19:31 -0700

    Update global.json file to specify sdk version as 2.2 to avoid build error "MSB4064  The 'DisableSdkPath' parameter is not supported by the 'Csc' task" when builds with 3.0 SDK

--- src/lib/Microsoft.Fx.Portability/Resources/LocalizedStrings.Designer.cs
+++ src/lib/Microsoft.Fx.Portability/Resources/LocalizedStrings.Designer.cs
@@ -10,7 +10,6 @@
 
 namespace Microsoft.Fx.Portability.Resources {
     using System;
-    using System.Reflection;
     
     
     /// <summary>
@@ -40,7 +39,7 @@ namespace Microsoft.Fx.Portability.Resources {
         public static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Microsoft.Fx.Portability.Resources.LocalizedStrings", typeof(LocalizedStrings).GetTypeInfo().Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Microsoft.Fx.Portability.Resources.LocalizedStrings", typeof(LocalizedStrings).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
