eecb2bc897ce472b8d35d7747adcebf39e5858b7
Alicia Li <alicial@microsoft.com>
2019-10-16 15:06:55 -0700

    updat the document link in excel report

--- src/lib/Microsoft.Fx.Portability.Reports.Excel/Resources/LocalizedStrings.Designer.cs
+++ src/lib/Microsoft.Fx.Portability.Reports.Excel/Resources/LocalizedStrings.Designer.cs
@@ -10,7 +10,6 @@
 
 namespace Microsoft.Fx.Portability.Reports.Excel.Resources {
     using System;
-    using System.Reflection;
     
     
     /// <summary>
@@ -20,7 +19,7 @@ namespace Microsoft.Fx.Portability.Reports.Excel.Resources {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class LocalizedStrings {
@@ -40,7 +39,7 @@ namespace Microsoft.Fx.Portability.Reports.Excel.Resources {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Microsoft.Fx.Portability.Reports.Excel.Resources.LocalizedStrings", typeof(LocalizedStrings).GetTypeInfo().Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Microsoft.Fx.Portability.Reports.Excel.Resources.LocalizedStrings", typeof(LocalizedStrings).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
