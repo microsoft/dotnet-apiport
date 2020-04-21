3bae7d9580c3138a9bc67b49666b9a6927f176e3
Andrew Arnott <andrewarnott@gmail.com>
2020-04-20 20:39:27 -0600

    Add script to install the .NET Core SDK
    This works on local dev boxes and on Azure Pipeline agents so that the SDK version we need is always installed on-demand.

---
+++ tools/Get-CatalogBin.ps1
@@ -0,0 +1,31 @@
+$ErrorActionPreference = "Stop"
+
+function DownloadFile($url, $outputPath) {
+	Write-Host "Attempt to download to $outputPath"
+
+	# If the file has been downloaded don't download again. An empty file implies a failed download
+	if(Test-Path $outputPath) {
+		$file = Get-ChildItem $outputPath
+
+		if($file.Length -gt 0) {
+			Write-Host "$outputPath is already downloaded"
+			return;
+		}
+	}
+
+	try {
+		# Create placeholder so directory exists
+		New-Item -Type File $OutputPath -Force | Out-Null
+
+		# Attempt to download.  If fails, placeholder remains so msbuild won't complain
+		Invoke-WebRequest $url -OutFile $OutputPath | Out-Null
+
+		Write-Host "Downloaded $OutputPath"
+	} catch {
+		Write-Error "Failed to download '$url'. $($Error[0])"
+	}
+}
+
+$address = "https://portability.blob.core.windows.net/catalog/catalog.bin"
+
+DownloadFile "$address" "$PSScriptRoot\..\.data\catalog.bin"
