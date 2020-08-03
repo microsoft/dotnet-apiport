39751f42a4e673921c03f997b616e0f7e30a7b97
Taylor Southwick <twsouthwick@outlook.com>
2020-08-03 10:53:39 -0700

    Remove .NET Framework build of ApiPort
    .NET Core 3.1 provides the APIs for web proxy that was the last remaining difference with .NET Framework. This change removes the .NET Framework build of ApiPort and enables proxy support using HttpClient.GetDefaultProxy. As part of this change, it simplifies the global generation as well.

--- src/ApiPort/ApiPort/Proxy/ProxyProvider.cs
+++ src/ApiPort/ApiPort/Proxy/ProxyProvider.cs
@@ -5,6 +5,7 @@ using Microsoft.Extensions.Configuration;
 using System;
 using System.Linq;
 using System.Net;
+using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -25,16 +26,6 @@ namespace Microsoft.Fx.Portability.Proxy
     /// </remarks>
     public class ProxyProvider : IProxyProvider
     {
-#if FEATURE_SYSTEM_PROXY
-        /// <summary>
-        /// Capture the default System Proxy so that it can be re-used.  Cannot rely on
-        /// WebRequest.DefaultWebProxy since someone can modify the DefaultWebProxy
-        /// property and we can't tell if it was modified and if we are still using System Proxy Settings or not.
-        /// One limitation of this method is that it does not look at the config file to get the defined proxy
-        /// settings.
-        /// </summary>
-        private static readonly IWebProxy _originalSystemProxy = WebRequest.GetSystemWebProxy();
-#endif
         private readonly IConfigurationRoot _configuration;
         private readonly CredentialsWrapper _credentialsWrapper;
         private readonly ICredentialProvider _credentialProvider;
@@ -58,7 +49,7 @@ namespace Microsoft.Fx.Portability.Proxy
         public IWebProxy GetProxy(Uri sourceUri)
         {
             // Check if the user has configured proxy details in settings or in the environment.
-            IWebProxy proxy = GetUserConfiguredProxy();
+            var proxy = GetWebProxyFromConfiguration();
 
             if (proxy != null)
             {
@@ -71,41 +62,7 @@ namespace Microsoft.Fx.Portability.Proxy
                 return proxy;
             }
 
-#if FEATURE_SYSTEM_PROXY
-            if (IsSystemProxySet(sourceUri))
-            {
-                proxy = GetSystemProxy(sourceUri);
-
-                if (proxy.Credentials != null)
-                {
-                    _credentialsWrapper.UpdateCredentials(proxy.Credentials);
-                }
-
-                proxy.Credentials = _credentialsWrapper;
-
-                return proxy;
-            }
-#endif
-
-            return null;
-        }
-
-        /// <summary>
-        /// Tries to get user configured proxy in the following order:
-        /// 1. Environment (configured in http_proxy, no proxy)
-        /// 2. Configuration file passed in.
-        /// </summary>
-        /// <returns>A web proxy or null if none was found.</returns>
-        private IWebProxy GetUserConfiguredProxy()
-        {
-            var proxy = GetWebProxyFromEnvironment();
-
-            if (proxy != null)
-            {
-                return proxy;
-            }
-
-            return GetWebProxyFromConfiguration();
+            return HttpClient.DefaultProxy;
         }
 
         public async Task<bool> TryUpdateCredentialsAsync(Uri uri, IWebProxy proxy, CredentialRequestType type, CancellationToken cancellationToken)
@@ -125,45 +82,6 @@ namespace Microsoft.Fx.Portability.Proxy
             else
             {
                 return false;
-            }
-        }
-
-        /// <summary>
-        /// Gets the proxy from environment variables.
-        /// </summary>
-        private static IWebProxy GetWebProxyFromEnvironment()
-        {
-            // Try reading from the environment variable http_proxy. This would be specified as http://<username>:<password>@proxy.com
-            var host = Environment.GetEnvironmentVariable(ProxyConstants.HttpProxy);
-            if (!string.IsNullOrEmpty(host)
-                && Uri.TryCreate(host, UriKind.Absolute, out var uri))
-            {
-                var webProxy = new ApiPort.Proxy.WebProxy(uri.GetComponents(UriComponents.HttpRequestUrl, UriFormat.SafeUnescaped));
-
-                if (!string.IsNullOrEmpty(uri.UserInfo))
-                {
-                    var credentials = uri.UserInfo.Split(':');
-
-                    if (credentials.Length > 1)
-                    {
-                        webProxy.Credentials = new NetworkCredential(
-                            userName: credentials[0],
-                            password: credentials[1]);
-                    }
-                }
-
-                var noProxy = Environment.GetEnvironmentVariable(ProxyConstants.NoProxy);
-                if (!string.IsNullOrEmpty(noProxy))
-                {
-                    // split comma-separated list of domains
-                    webProxy.BypassList = noProxy.Split(new[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
-                }
-
-                return webProxy;
-            }
-            else
-            {
-                return null;
             }
         }
 
@@ -210,48 +128,6 @@ namespace Microsoft.Fx.Portability.Proxy
 
             return configuration.GetValue<T>(key);
         }
-
-#if FEATURE_SYSTEM_PROXY
-        private static ApiPort.Proxy.WebProxy GetSystemProxy(Uri uri)
-        {
-            // WebRequest.DefaultWebProxy seems to be more capable in terms of getting the default
-            // proxy settings instead of the WebRequest.GetSystemProxy()
-            var proxyUri = _originalSystemProxy.GetProxy(uri);
-            return new ApiPort.Proxy.WebProxy(proxyUri);
-        }
-
-        /// <summary>
-        /// Return true or false if connecting through a proxy server.
-        /// </summary>
-        private static bool IsSystemProxySet(Uri uri)
-        {
-            // The reason for not calling the GetSystemProxy is because the object
-            // that will be returned is no longer going to be the proxy that is set by the settings
-            // on the users machine only the Address is going to be the same.
-            // Not sure why the .NET team did not want to expose all of the useful settings like
-            // ByPass list and other settings that we can't get because of it.
-            // Anyway the reason why we need the DefaultWebProxy is to see if the uri that we are
-            // getting the proxy for to should be bypassed or not. If it should be bypassed then
-            // return that we don't need a proxy and we should try to connect directly.
-            var proxy = WebRequest.DefaultWebProxy;
-            if (proxy != null)
-            {
-                var proxyUri = proxy.GetProxy(uri);
-                if (proxyUri != null)
-                {
-                    var proxyAddress = new Uri(proxyUri.AbsoluteUri);
-                    if (string.Equals(proxyAddress.AbsoluteUri, uri.AbsoluteUri, StringComparison.OrdinalIgnoreCase))
-                    {
-                        return false;
-                    }
-
-                    return !proxy.IsBypassed(uri);
-                }
-            }
-
-            return false;
-        }
-#endif
 
         /// <summary>
         /// Helper class that wraps NetworkCredentials so the proxy always
