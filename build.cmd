43a1f665823dc7aea1ff53a86240c13c5016fdb6
Matt Mitchell <mmitche@microsoft.com>
2021-03-10 13:42:30 -0800

    Revert "Test PR"
    This reverts commit a8245372c878068705495eaba9ba1072670b3219.

--- build.cmd
+++ build.cmd
@@ -3,5 +3,4 @@
 :: Default to a release build
 IF "%Configuration%"=="" SET Configuration=Release
 
-dir \\mmitche1\public\darcwithmultibuild
 powershell -noprofile -executionpolicy bypass -file .\build.ps1 %Configuration% "AnyCPU" -RunTests
