a8245372c878068705495eaba9ba1072670b3219
Matt Mitchell <mmitche@microsoft.com>
2021-03-10 13:40:33 -0800

    Test PR

--- build.cmd
+++ build.cmd
@@ -3,4 +3,5 @@
 :: Default to a release build
 IF "%Configuration%"=="" SET Configuration=Release
 
+dir \\mmitche1\public\darcwithmultibuild
 powershell -noprofile -executionpolicy bypass -file .\build.ps1 %Configuration% "AnyCPU" -RunTests
