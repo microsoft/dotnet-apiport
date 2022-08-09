4ed4024f76c02e8c3ebc6007bb08b044af1d9ea3
Brandon H <hurlburb@microsoft.com>
2022-08-09 08:42:06 -0700

    Replacing Online analysis with Offline
    Stub out the current ApiPortService for the OfflineApiPortService
    along with the necessary injection objects. This successfully
    analyzes an assembly with the host machine disconnected from
    the internet.

---
+++ src/ApiPort/ApiPort.VisualStudio/DependencyBuilder.Offline.cs
@@ -0,0 +1,64 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Autofac;
+
+using Microsoft.Fx.Portability;
+using Microsoft.Fx.Portability.Analysis;
+using Microsoft.Fx.Portability.Analyzer;
+using Microsoft.Fx.Portability.Reporting;
+
+using System;
+using System.IO;
+using System.Reflection;
+
+namespace ApiPort
+{
+    internal static class DependencyBuilder
+    {
+        internal const string DefaultOutputFormatInstanceName = "DefaultOutputFormat";
+
+        public static void RegisterOfflineModule(this ContainerBuilder builder)
+        {
+            builder.RegisterType<DependencyOrderer>()
+                .As<IDependencyOrderer>()
+                .SingleInstance();
+
+            builder.RegisterType<RequestAnalyzer>()
+                .As<IRequestAnalyzer>()
+                .SingleInstance();
+
+            builder.RegisterType<AnalysisEngine>()
+                .As<IAnalysisEngine>()
+                .SingleInstance();
+
+            builder.RegisterModule(new OfflineDataModule(DefaultOutputFormatInstanceName));
+            LoadReportWriters(builder);
+        }
+
+        private static void LoadReportWriters(ContainerBuilder builder)
+        {
+            foreach (var path in Directory.EnumerateFiles(GetApplicationDirectory(), "Microsoft.Fx.Portability.Reports.*.dll"))
+            {
+                try
+                {
+                    var name = new AssemblyName(Path.GetFileNameWithoutExtension(path));
+                    var assembly = Assembly.Load(name);
+
+                    builder.RegisterAssemblyTypes(assembly)
+                        .AssignableTo<IReportWriter>()
+                        .As<IReportWriter>()
+                        .SingleInstance();
+                }
+                catch (Exception)
+                {
+                }
+            }
+        }
+
+        private static string GetApplicationDirectory()
+        {
+            return Path.GetDirectoryName(typeof(DependencyBuilder).GetTypeInfo().Assembly.Location);
+        }
+    }
+}
